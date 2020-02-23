f1d66571197d4bb38cd2263affda6c7e274bd60a
Érick Firmo <34639603+erickfirmo@users.noreply.github.com>
2020-02-23 01:09:46 -0300

    Create CartaoMagnetico.php

---
+++ app/CartaoMagnetico.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartaoMagnetico extends Model
+{
+    public $table = 'cartoes_magneticos';
+
+    public $fillable = [
+        'categoria',
+        'codigo',
+        'responsavel_id',
+        'status',
+    ];
+}
