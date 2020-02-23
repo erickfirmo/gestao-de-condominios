4b997360b4c1965e8418a43a8cb3f73e00f29c8d
Érick Firmo <34639603+erickfirmo@users.noreply.github.com>
2020-02-23 15:21:10 -0300

    Create 'imoveis' migration

---
+++ database/migrations/2020_02_23_153003_create_imoveis_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImoveisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imoveis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('numero', 8);
+            $table->string('bloco', 10);
+            $table->string('andar', 3);
+            $table->string('descricao', 200);
+            $table->string('observacoes', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imoveis');
+    }
+}
