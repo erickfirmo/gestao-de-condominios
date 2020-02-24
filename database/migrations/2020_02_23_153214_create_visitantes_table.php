516db570b4c6d4ecd4998594ebfc0f4cf2f47f78
Érick Firmo <erickluques@gmail.com>
2020-02-23 21:13:18 -0300

    Create 'visitantes' migration

---
+++ database/migrations/2020_02_23_153214_create_visitantes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visitantes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome', 40);
+
+            $table->string('chegada');
+            $table->string('saida');
+            $table->string('transporte');
+            $table->unsignedBigInteger('morador_responsavel_id');
+
+            $table->foreign('morador_responsavel_id')
+                ->references('id')
+                ->on('moradores');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visitantes');
+    }
+}
