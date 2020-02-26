96fbded6c1a51df2e0284dba3da210b23102d2f7
Érick Firmo <erickluques@gmail.com>
2020-02-26 16:35:01 -0300

    Create 'empresa has many funcionarios' relationship method in Empresa.php

--- app/Models/Empresa.php
+++ app/Models/Empresa.php
@@ -15,11 +15,16 @@ class Empresa extends Model
         'email',
         'telefone_1',
         'telefone_2',
-        'responsavel_para_contato'
+        'responsavel_para_contato',
     ];
     
     public function condominios()
     {
         return $this->hasMany(Condominio::class, 'empresa_id', 'id');
+    }
+
+    public function funcionarios()
+    {
+        return $this->hasMany(Funcionario::class, 'empresa_id', 'id');
     }
 }
