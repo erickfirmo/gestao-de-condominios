26d23601749ba53b0a93c60ad13d91b6994c6248
Érick Firmo <erickluques@gmail.com>
2020-03-03 00:27:07 -0300

    Route resource 'empresas' - superadmin

--- routes/web.php
+++ routes/web.php
@@ -10,17 +10,14 @@ require_once 'auth/user.php';
 require_once 'auth/admin.php';
 require_once 'auth/superadmin.php';
 
+
+Route::prefix('superadmin')->name('superadmin.')->group(function () {
+    Route::resource('empresas', 'EmpresaController');
+        
+});
+
+
 Route::prefix('admin')->name('admin.')->namespace('')->group(function () {
-
-    Route::prefix('examples')->name('examples.')->group(function () {
-        Route::get('/', 'ExampleController@index')->name('index');
-        Route::get('/create', 'ExampleController@create')->name('create');
-        Route::get('/{id}/edit', 'ExampleController@edit')->name('edit');
-        Route::post('/store', 'ExampleController@store')->name('store');
-        Route::put('/{id}', 'ExampleController@update')->name('update');
-        Route::delete('/{id}', 'ExampleController@destroy')->name('destroy');
-
-    });
 
 });
 
