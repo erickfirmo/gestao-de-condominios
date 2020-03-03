3e9b4d30fc71b4de610b28f488aad7be562f4c39
Érick Firmo <erickluques@gmail.com>
2020-03-03 00:36:11 -0300

    Index method in EmpresaController.php

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Empresa;
 
 class EmpresaController extends Controller
 {
@@ -13,7 +14,7 @@ class EmpresaController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(['empresas' => Empresa::all()]);
     }
 
     /**
@@ -34,7 +35,7 @@ class EmpresaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
