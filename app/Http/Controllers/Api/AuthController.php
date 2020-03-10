bf29bfa3f2a6cb96ab4f25984c9bb7e4f097cb3a
Érick Firmo <erickluques@gmail.com>
2020-03-09 22:42:42 -0300

    Register method in AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,5 +9,26 @@ use App\User;
 
 class AuthController extends Controller
 {
-    //
+    public function register (Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+    
+        if ($validator->fails())
+        {
+            return response(['errors'=>$validator->errors()->all()], 422);
+        }
+    
+        $request['password']=Hash::make($request['password']);
+        $user = User::create($request->toArray());
+    
+        $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+        $response = ['token' => $token];
+    
+        return response($response, 200);
+    
+    }
 }
