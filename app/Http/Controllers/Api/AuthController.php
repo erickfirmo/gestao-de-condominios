d14a2f8fa8ca1d9ec12a90978dd37d3794c3174e
Érick Firmo <erickluques@gmail.com>
2020-03-09 22:41:57 -0300

    Update AuthController.php

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\User;
 
 class AuthController extends Controller
 {
