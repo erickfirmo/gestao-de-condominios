fb80d42023d16b3a18538a566f0f3c8e414ab0cb
Érick Firmo <erickluques@gmail.com>
2020-03-10 22:24:02 -0300

    Update AuthServiceProvider.php

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -28,7 +28,11 @@ class AuthServiceProvider extends ServiceProvider
         
         Passport::routes();
 
-        Passport::loadKeysFrom('/secret-keys/oauth');
+        Passport::tokensExpireIn(now()->addDays(15));
+
+        Passport::refreshTokensExpireIn(now()->addDays(30));
+
+        Passport::personalAccessTokensExpireIn(now()->addMonths(6));
 
     }
 }
