a30560d93a96b28e696a8275c5788c422104756a
Érick Firmo <erickluques@gmail.com>
2020-03-18 23:07:39 -0300

    Update functions.php

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,22 +1,27 @@
 <?php
 
-function classActivePath($path, $activeClass, $n)
-{
-    if($n != 0) {
-        $i = 0;
-        $final_path = '';
-        $explode_path = explode('/', $_SERVER['REQUEST_URI']);
-        while ($i != $n)
-        {
-            $i++;
-            if(isset($explode_path[$i]))
-                $final_path .= '/'.$explode_path[$i];
-        }
-        if($path == $final_path)
-            echo " $activeClass";
 
-    } else {
-        if($_SERVER['REQUEST_URI'] == $path)
-            echo " $activeClass";
+
+if(!function_exists('classActivePath'))
+{
+    function classActivePath($path, $activeClass, $n)
+    {
+        if($n != 0) {
+            $i = 0;
+            $final_path = '';
+            $explode_path = explode('/', $_SERVER['REQUEST_URI']);
+            while ($i != $n)
+            {
+                $i++;
+                if(isset($explode_path[$i]))
+                    $final_path .= '/'.$explode_path[$i];
+            }
+            if($path == $final_path)
+                echo " $activeClass";
+    
+        } else {
+            if($_SERVER['REQUEST_URI'] == $path)
+                echo " $activeClass";
+        }
     }
 }
