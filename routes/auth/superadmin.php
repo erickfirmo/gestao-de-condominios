302c22f5799bbf51629fca8b20005634e988e2e2
Érick Firmo <erickluques@gmail.com>
2020-03-19 01:47:17 -0300

    Update superadmin.php

--- routes/auth/superadmin.php
+++ routes/auth/superadmin.php
@@ -1,13 +1,13 @@
 <?php
 
-Route::prefix('superadmin')->name('superadmin.')->namespace('Superadmin')->group(function () {
+Route::prefix('superadmin')->name('superadmin.')->namespace('superadmin')->group(function () {
     Auth::routes();
     Route::middleware('auth:superadmin')->group(function () {
         Route::get('/', 'HomeController@index')->name('/');
         Route::get('home', 'HomeController@home')->name('home');
-        Route::get('register', 'HomeController@index');
-
     });
+    Route::get('edit/{id}', 'Auth\EditController@edit')->name('edit');
+    Route::post('update/{id}', 'Auth\EditController@update')->name('update');
 
     Route::get('/password/email', function () {
         return redirect('superadmin/login');
