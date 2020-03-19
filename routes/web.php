d732c64424add91824aacc119d2fc179bcf26547
Érick Firmo <erickluques@gmail.com>
2020-03-18 21:56:31 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,58 @@ require_once 'auth/superadmin.php';
 
 
 Route::prefix('superadmin')->name('superadmin.')->group(function () {
+    // cadastros
     Route::resource('empresas', 'EmpresaController');
     Route::resource('condominios', 'CondominioController');
 
+    // usuarios
+
+
+    // financeiros
+
+
+    // portarias
+
         
 });
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    // cadastros
+    Route::resource('empresas', 'EmpresaController');
+    Route::resource('condominios', 'CondominioController');
+
+    // usuarios
+
+
+    // financeiros
+
+
+    // portarias
+
+        
+});
+
+// users
+Route::prefix('user')->name('user.')->group(function () {
+    // cadastros
+    Route::prefix('user')->name('user.')->group(function () {
+        // moradores
+        Route::resource('moradores', 'MoradorController');
+            
+    });
+
+    // usuarios
+
+
+    // financeiros
+
+
+    // portarias
+
+        
+});
+
 
 
 Auth::routes();
