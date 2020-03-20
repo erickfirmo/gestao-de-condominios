04bdabd8581e2a0159c406ec7a847f2c68170555
Érick Firmo <erickluques@gmail.com>
2020-03-20 07:12:26 -0300

    Update create.blade.php

--- resources/views/cadastros/empresas/create.blade.php
+++ resources/views/cadastros/empresas/create.blade.php
@@ -55,11 +55,22 @@
                             <div class="form-group row">
                                     <span class="label-text col-md-2 col-form-label text-md-right">Nome Fantasia</span>
                                 <div class="col-md-10">
-                                    <input type="text" name="nome_fantasia" class="form-control" id="nome_fantasia" maxlenght="30" value="{{ old('nome_fantasia') }}">
+                                    <input type="text" name="nome_fantasia" class="form-control" id="nome_fantasia" maxlenght="60" value="{{ old('nome_fantasia') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                    <span class="label-text col-md-2 col-form-label text-md-right">CNPJ</span>
+                                <div class="col-md-10">
+                                    <input type="text" name="cnpj" class="form-control" id="cnpj" maxlenght="30" value="{{ old('cnpj') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+
+                            
                             <div class="row">
                                 <div class="col-lg-10 offset-lg-2">
                                     <input type="submit" value="Salvar" class="btn btn-sm btn-rounded btn-success">
