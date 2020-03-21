723476feb14a6761ff9c7dd18321ffb1649ce9fa
Érick Firmo <erickluques@gmail.com>
2020-03-21 03:07:21 -0300

    Update 2020_02_23_153042_create_pets_table.php

--- database/migrations/2020_02_23_153042_create_pets_table.php
+++ database/migrations/2020_02_23_153042_create_pets_table.php
@@ -25,7 +25,8 @@ class CreatePetsTable extends Migration
 
             $table->foreign('morador_id')
                 ->references('id')
-                ->on('moradores');
+                ->on('moradores')
+                ->onDelete('cascade');
         });
     }
 
