cba076fd73bc9f22b19a9a8eb32b0a615a87f005
Érick Firmo <erickluques@gmail.com>
2020-03-20 22:41:39 -0300

    Update _footer.blade.php

--- resources/views/superadmin/partials/_footer.blade.php
+++ resources/views/superadmin/partials/_footer.blade.php
@@ -1,5 +1,5 @@
 <!-- Main Footer Start -->
 <footer class="main--footer main--footer-light">
-        <p>Copyright &copy; <a href="https://github.com/erickfirmo">Érick Firmo</a>. Todos os Direitos Reservados.</p>
-    </footer>
-    <!-- Main Footer End -->
+    <p>Copyright &copy; <a href="https://github.com/erickfirmo">Gestão de Condomínios</a>. Todos os Direitos Reservados.</p>
+</footer>
+<!-- Main Footer End -->
