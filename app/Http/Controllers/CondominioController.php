eac48fd506a2042d1b57bb78b8e416631dd19606
Érick Firmo <erickluques@gmail.com>
2020-03-21 02:32:01 -0300

    Update CondominioController.php

--- app/Http/Controllers/CondominioController.php
+++ app/Http/Controllers/CondominioController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Condominio;
 
 class CondominioController extends Controller
 {
-    /**
+    public function __construct()
+    {
+        return $this->middleware('auth:superadmin');
+    }   
+
+     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        //
+        return view('cadastros.condominios.index', [
+            'condominios' => Condominio::all()
+        ]);
     }
 
     /**
@@ -23,7 +31,7 @@ class CondominioController extends Controller
      */
     public function create()
     {
-        //
+        return view('cadastros.condominios.create');
     }
 
     /**
@@ -34,7 +42,49 @@ class CondominioController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nome' => 'required|min:1|max:60|unique:condominios',
+            'descricao' => 'required|min:1|max:60',
+            'cep' => 'required|digits:18',
+            'logradouro' => 'required|min:3|max:40|',
+            'numero' => 'required|min:8|max:20',
+            'bairro' => 'min:8|max:20',
+            'cidade' => 'required|min:1|max:50',
+            'uf_id' => 'required|min:1|max:50',
+            'complemento' => 'required|min:1|max:50',
+            'observacoes' => 'required|min:1|max:50',
+            'empresa_id' => 'required|min:1|max:50',
+        ]);
+
+        $nome = $request->input('nome');
+        $descricao = $request->input('descricao');
+        $cep = $request->input('cep');
+        $logradouro = $request->input('logradouro');
+        $numero = $request->input('numero');
+        $bairro = $request->input('bairro');
+        $cidade = $request->input('cidade');
+        $uf_id = $request->input('uf_id');
+        $complemento = $request->input('complemento');
+        $observacoes = $request->input('observacoes');
+        $empresa_id = $request->input('empresa_id');
+
+        
+        $condominio = new Condominio;
+        $condominio->nome = $nome;
+        $condominio->descricao = $descricao;
+        $condominio->cep = $cep;
+        $condominio->logradouro = $logradouro;
+        $condominio->numero = $numero;
+        $condominio->bairro = $bairro;
+        $condominio->cidade = $cidade;
+        $condominio->uf_id = $uf_id;
+        $condominio->complemento = $complemento;
+        $condominio->observacoes = $observacoes;
+        $condominio->empresa_id = $empresa_id;
+        $condominio->save();
+
+        return redirect()->route('superadmin.condominios.edit', compact('condominio'))
+            ->with('success', 'Condomínio cadastrado com sucesso!');
     }
 
     /**
@@ -45,7 +95,9 @@ class CondominioController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('cadastros.condominios.show', [
+            'condominio' => Condominio::findOrFail($id)
+        ]);
     }
 
     /**
@@ -56,7 +108,9 @@ class CondominioController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('cadastros.condominios.edit', [
+            'condominio' => Condominio::findOrFail($id)
+        ]);
     }
 
     /**
@@ -68,7 +122,28 @@ class CondominioController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'razao_social' => 'required|min:1|max:60|unique:condominios,razao_social,'.$id,
+            'nome_fantasia' => 'required|min:1|max:60',
+            'cnpj' => 'required|unique:condominios,cnpj,'.$id,
+            'email' => 'required|min:3|max:40|',
+            'telefone_1' => 'required|min:8|max:20',
+            'telefone_2' => 'min:8|max:20',
+            'responsavel_para_contato' => 'required|min:1|max:50',
+        ]);
+
+        $condominio = Condominio::findOrFail($id)->update([
+            'razao_social' => $request->razao_social,
+            'nome_fantasia' => $request->nome_fantasia,
+            'cnpj' => $request->cnpj,
+            'email' => $request->email,
+            'telefone_1' => $request->telefone_1,
+            'telefone_2' => $request->telefone_2,
+            'responsavel_para_contato' => $request->responsavel_para_contato,
+        ]);
+
+        return redirect()->route('superadmin.condominios.edit', compact('condominio'))
+            ->with('success', 'Dados da condominio atualizados com sucesso!');
     }
 
     /**
@@ -79,6 +154,9 @@ class CondominioController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Condominio::findOrFail($id)->delete();
+
+        return redirect()->route('superadmin.condominios.index')
+            ->with('success', 'Condominio removida com sucesso!');
     }
 }
