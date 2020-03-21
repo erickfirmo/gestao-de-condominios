13460804b9136abc3bc69aeafd570f9b0808ed5e
Érick Firmo <erickluques@gmail.com>
2020-03-21 03:07:37 -0300

    Update 2020_02_23_153245_create_entregas_table.php

--- database/migrations/2020_02_23_153245_create_entregas_table.php
+++ database/migrations/2020_02_23_153245_create_entregas_table.php
@@ -23,7 +23,8 @@ class CreateEntregasTable extends Migration
             $table->unsignedBigInteger('morador_id');
             $table->foreign('morador_id')
                 ->references('id')
-                ->on('moradores');
+                ->on('moradores')
+                ->onDelete('cascade');;
                 
             $table->timestamps();
         });
