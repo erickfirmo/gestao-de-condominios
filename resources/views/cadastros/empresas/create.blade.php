56b073c01ed4b73cbc9c70e40164c1b0c6023b58
Érick Firmo <erickluques@gmail.com>
2020-03-21 10:23:15 -0300

    Update create.blade.php

--- resources/views/cadastros/empresas/create.blade.php
+++ resources/views/cadastros/empresas/create.blade.php
@@ -26,6 +26,8 @@
     </section>
     <!-- Page Header End -->
 
+    @include('superadmin.partials._alert')
+
 
     <!-- Main Content Start -->
     <section class="main--content">
@@ -38,58 +40,62 @@
                     </div>
 
                     <div class="panel-content">
-                    @include('superadmin.partials._alert')
                     
                         <form action="{{ route('superadmin.empresas.store') }}" method="POST">
                             @csrf
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                    <span class="label-text col-md-2 col-form-label text-md-right">Razão Social</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Razão Social</span>
                                 <div class="col-md-10">
-                                    <input type="text" name="razao_social" class="form-control" id="razao_social" maxlenght="60" value="{{ old('razao_social') }}">
+                                    <span class="form-text text-error">{{ $errors->first('razao_social') ? $errors->first('razao_social') : '' }}</span>
+                                    <input type="text" name="razao_social" class="form-control{{ $errors->first('razao_social') ? ' error' : '' }}" id="razao_social" maxlenght="60" value="{{ old('razao_social') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                    <span class="label-text col-md-2 col-form-label text-md-right">Nome Fantasia</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Nome Fantasia</span>
                                 <div class="col-md-10">
-                                    <input type="text" name="nome_fantasia" class="form-control" id="nome_fantasia" maxlenght="60" value="{{ old('nome_fantasia') }}">
+                                    <span class="form-text text-error">{{ $errors->first('nome_fantasia') ? $errors->first('nome_fantasia') : '' }}</span>
+                                    <input type="text" name="nome_fantasia" class="form-control{{ $errors->first('nome_fantasia') ? ' error' : '' }}" id="nome_fantasia" maxlenght="60" value="{{ old('nome_fantasia') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                    <span class="label-text col-md-2 col-form-label text-md-right">Cnpj</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Cnpj</span>
                                 <div class="col-md-10">
-                                    <input type="text" name="cnpj" class="form-control" id="cnpj" maxlenght="18" value="{{ old('cnpj') }}">
+                                    <span class="form-text text-error">{{ $errors->first('cnpj') ? $errors->first('cnpj') : '' }}</span>
+                                    <input type="text" name="cnpj" class="form-control{{ $errors->first('cnpj') ? ' error' : '' }}" id="cnpj" maxlenght="18" value="{{ old('cnpj') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                    <span class="label-text col-md-2 col-form-label text-md-right">Email</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Email</span>
                                 <div class="col-md-10">
-                                    <input type="email" name="email" class="form-control" id="email" maxlenght="40" value="{{ old('email') }}">
+                                    <span class="form-text text-error">{{ $errors->first('email') ? $errors->first('email') : '' }}</span>
+                                    <input type="email" name="email" class="form-control{{ $errors->first('email') ? ' error' : '' }}" id="email" maxlenght="40" value="{{ old('email') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
                             
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                    <span class="label-text col-md-2 col-form-label text-md-right">Telefone 1</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Telefone 1</span>
                                 <div class="col-md-10">
-                                    <input type="text" name="telefone_1" class="form-control" id="telefone_1" maxlenght="20" value="{{ old('telefone_1') }}">
+                                    <span class="form-text text-error">{{ $errors->first('telefone_1') ? $errors->first('telefone_1') : '' }}</span>
+                                    <input type="text" name="telefone_1" class="form-control{{ $errors->first('telefone_1') ? ' error' : '' }}" id="telefone_1" maxlenght="20" value="{{ old('telefone_1') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                    <span class="label-text col-md-2 col-form-label text-md-right">Telefone 2</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Telefone 2</span>
                                 <div class="col-md-10">
                                     <input type="text" name="telefone_2" class="form-control" id="telefone_2" maxlenght="20" value="{{ old('telefone_2') }}">
                                 </div>
@@ -98,9 +104,10 @@
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                    <span class="label-text col-md-2 col-form-label text-md-right">Responsavel</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Responsável</span>
                                 <div class="col-md-10">
-                                    <input type="text" name="responsavel_para_contato" class="form-control" id="responsavel_para_contato" maxlenght="50" value="{{ old('responsavel_para_contato') }}">
+                                    <span class="form-text text-error">{{ $errors->first('responsavel_para_contato') ? $errors->first('responsavel_para_contato') : '' }}</span>
+                                    <input type="text" name="responsavel_para_contato" class="form-control{{ $errors->first('responsavel_para_contato') ? ' error' : '' }}" id="responsavel_para_contato" maxlenght="50" value="{{ old('responsavel_para_contato') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
