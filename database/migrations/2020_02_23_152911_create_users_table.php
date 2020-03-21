699defbfaab5fcb020b8ea997500320a958d7373
Érick Firmo <erickluques@gmail.com>
2020-03-21 03:07:10 -0300

    Update 2020_02_23_152911_create_users_table.php

--- database/migrations/2020_02_23_152911_create_users_table.php
+++ database/migrations/2020_02_23_152911_create_users_table.php
@@ -18,18 +18,12 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('foto_de_perfil');
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
 
             $table->unsignedBigInteger('funcionario_id');
             $table->foreign('funcionario_id')
                 ->references('id')
                 ->on('funcionarios');
-
-            $table->string('api_token', 80)
-                ->unique()
-                ->nullable()
-                ->default(null);
 
             $table->rememberToken();
             $table->timestamps();
