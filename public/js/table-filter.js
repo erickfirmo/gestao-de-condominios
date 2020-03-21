0b848916dd418e081ce3a81f00157ece828fe914
Érick Firmo <erickluques@gmail.com>
2020-03-20 23:38:48 -0300

    Update table-filter.js

--- public/js/table-filter.js
+++ public/js/table-filter.js
@@ -1,18 +1,20 @@
 function tableFilter(table_class, input) {
-    var filter, table, tr, td, i, txtValue;
+    var filter, table, tr, tds, has_data, i, txtValue;
     filter = input.value.toUpperCase();
     table = document.getElementsByClassName(table_class)[0];
     tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+    for (i = 1; i < tr.length; i++) {
+        tds = tr[i].getElementsByTagName("td");
+        for (j = 0; j < tds.length; j++) {
+            if (tds[j]) {
+                txtValue = tds[j].textContent || tds[j].innerText;
+                if(has_data != 'y')
+                {
+                    has_data = txtValue.toUpperCase().indexOf(filter) > -1 ? 'y' : 'n';
+                }
             }
-
         }
+            tr[i].style.display = (has_data == 'y') ? "" : "none";
+            has_data = false;
     }
 }
