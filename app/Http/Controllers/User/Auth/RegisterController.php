931ff67c8cd0be9da7a99f053887a4cdd4a939f9
Érick Firmo <erickluques@gmail.com>
2020-03-27 01:29:48 -0300

    Update RegisterController.php

--- app/Http/Controllers/User/Auth/RegisterController.php
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -39,7 +39,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:superadmin');
+        $this->middleware('auth:admin');
     }
 
 
