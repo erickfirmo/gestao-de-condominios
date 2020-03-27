c86c6c1f1907e3832b0858e7cc129f6ed989e784
Érick Firmo <erickluques@gmail.com>
2020-03-27 04:34:46 -0300

    Update alerts.js

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -27,15 +27,4 @@
       )
     }
 });
-
-
-$('.remove-form').on('submit', function() {
-    $.post($(this).data('token'),
-    {
-        name: "Donald Duck",
-        city: "Duckburg"
-    },
-    function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-    });
-});*/
+*/
