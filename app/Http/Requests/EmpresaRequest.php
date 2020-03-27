34b93dcd28975df3ebe03cd6cd54291fb60a38ed
Érick Firmo <erickluques@gmail.com>
2020-03-27 02:57:25 -0300

    Update EmpresaRequest.php

--- app/Http/Requests/EmpresaRequest.php
+++ app/Http/Requests/EmpresaRequest.php
@@ -6,9 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class EmpresaRequest extends FormRequest
 {
-    private $store_rules = [];
-
-    private $update_rules = [];
     /**
      * Determine if the user is authorized to make this request.
      *
@@ -19,10 +16,22 @@ class EmpresaRequest extends FormRequest
         return true;
     }
 
-    
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
     public function rules()
     {
-        return $this->getRules;
+        return [
+            'razao_social' => 'required|min:1|max:60|unique:empresas,razao_social,'.$id,
+            'nome_fantasia' => 'required|min:1|max:60',
+            'cnpj' => 'required|unique:empresas,cnpj,'.$id,
+            'email' => 'required|min:3|max:40|',
+            'telefone_1' => 'required|min:8|max:20',
+            'telefone_2' => 'min:8|max:20',
+            'responsavel_para_contato' => 'required|min:1|max:50',
+        ];
     }
 
     public function attributes()
