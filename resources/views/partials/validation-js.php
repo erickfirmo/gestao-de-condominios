295712fde1f87aa6805143b4c0c72bfdd46fd1b8
Érick Firmo <erickluques@gmail.com>
2020-03-27 03:18:47 -0300

    Delete validation-js.php

--- resources/views/partials/validation-js.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$asp = "'";
-echo "<script>\n";
-    foreach($errors->all() as $key => $error){
-        echo '$("input'.'[name='.$asp.$errors->keys()[$key].$asp.']'.'")'.'.addClass("error");'."\n";
-        echo '$("input'.'[name='.$asp.$errors->keys()[$key].$asp.']'.'")'.'.prev().text("'.$error.'");'."\n";
-    }
-echo "</script>\n";
