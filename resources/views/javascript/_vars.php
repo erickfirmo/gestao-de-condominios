cf96bef478381ac598f2755a21c87d003c109e0d
Érick Firmo <erickluques@gmail.com>
2020-03-27 05:02:21 -0300

    Update _vars.php

--- resources/views/javascript/_vars.php
+++ resources/views/javascript/_vars.php
@@ -5,5 +5,7 @@ echo "<script>\n";
     foreach($errors->all() as $key => $error){
         $errors_response[$errors->keys()[$key]] = $error;
     }
-    echo 'var errors_response = '.json_encode($errors_response).';';
+    echo "var errors_response = ".json_encode($errors_response).";\n";
+    echo "var success_response = '".session()->get('success')."';\n";
+    
 echo "</script>\n";
