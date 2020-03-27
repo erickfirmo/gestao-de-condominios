c60ccad68a5978eb772669ec4eb9c742d0e6a7f4
Érick Firmo <erickluques@gmail.com>
2020-03-27 04:10:03 -0300

    Update _vars.php

--- resources/views/javascript/_vars.php
+++ resources/views/javascript/_vars.php
@@ -1,12 +1,9 @@
 <?php
-
 $asp = "'";
 $errors_response = [];
 echo "<script>\n";
-
     foreach($errors->all() as $key => $error){
         $errors_response[$errors->keys()[$key]] = $error;
     }
     echo 'var errors_response = '.json_encode($errors_response).';';
-
 echo "</script>\n";
