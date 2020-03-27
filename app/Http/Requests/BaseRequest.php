4780e46c2cace3b3c1649cf665cd0f9593bacda2
Érick Firmo <erickluques@gmail.com>
2020-03-27 01:41:01 -0300

    Create BaseRequest.php

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class BaseRequest extends FormRequest
+{
+    public function checkRules()
+    {
+        return true;
+    }
+
+}
