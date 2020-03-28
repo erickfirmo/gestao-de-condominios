e688468f14e7f9d921e1ed5f645cfa34fc6b4926
Érick Firmo <erickluques@gmail.com>
2020-03-28 13:26:29 -0300

    Create RolesAuth.php

---
+++ app/Http/Middleware/RolesAuth.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class RolesAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        dd(Auth::user()->role_id);
+
+        $role = \App\Models\Role::findOrFail(auth()->user()->role_id);
+        $permissions = $role->permissions;
+        $actionName = class_basename($request->route()->getActionname());
+
+        foreach ($permissions as $permission)
+        {
+            $_namespaces_chunks = explode('\\', $permission->controller);
+            $controller = end($_namespaces_chunks);
+            if ($actionName == $controller . '@' . $permission->method)
+            {
+                return $next($request);
+            }
+        }
+
+        return response('Unauthorized Action', 403);
+    }
+}
