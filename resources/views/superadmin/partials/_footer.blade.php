67dff3e2061f70b500c8897b60885cc3a55b36a3
Érick Firmo <erickluques@gmail.com>
2020-03-28 14:04:27 -0300

    Remove some files

--- resources/views/superadmin/partials/_footer.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- Main Footer Start -->
-<footer class="main--footer main--footer-light">
-    <p>&copy; <a href="https://github.com/erickfirmo">Gestão de Condomínios</a>. Todos os Direitos Reservados.</p>
-</footer>
-<!-- Main Footer End -->
