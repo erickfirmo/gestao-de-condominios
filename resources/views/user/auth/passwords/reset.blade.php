2168c1feb2958901a6bac465ff0b91fdf5c0f8d5
Érick Firmo <erickluques@gmail.com>
2020-03-28 14:06:01 -0300

    Update reset.blade.php

--- resources/views/user/auth/passwords/reset.blade.php
+++ resources/views/user/auth/passwords/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.user')
+@extends('layouts.dadmin')
 
 @section('content')
 <div class="container">
@@ -8,7 +8,7 @@
                 <div class="card-header">{{ __('Redefinir Senha') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('user.password.update') }}">
+                    <form method="POST" action="{{ route('password.update') }}">
                         @csrf
 
                         <input type="hidden" name="token" value="{{ $token }}">
