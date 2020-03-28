8940d30d23837c954dd1a8e1a54761eeae6a5194
Érick Firmo <erickluques@gmail.com>
2020-03-28 14:04:40 -0300

    Create _footer.blade.php

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<!-- Main Footer Start -->
+<footer class="main--footer main--footer-light">
+    <p>&copy; <a href="https://github.com/erickfirmo">Gestão de Condomínios</a>. Todos os Direitos Reservados.</p>
+</footer>
+<!-- Main Footer End -->
