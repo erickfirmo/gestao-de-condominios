0621f379a6e912814c63f37c9548c8527b4bb3b9
Érick Firmo <erickluques@gmail.com>
2020-03-28 14:04:49 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,21 @@
 <?php
 
 Route::get('/', function () {
-    return redirect('/user/login');
+    return redirect('/login');
 });
 
 Auth::routes();
 
 require_once 'auth/user.php';
 require_once 'auth/admin.php';
-require_once 'auth/superadmin.php';
 
-Route::prefix('superadmin')->name('superadmin.')->group(function () {
+Route::prefix('admin')->name('admin.')->group(function () {
 
     ##cadastros
     // empresas
-    /*Route::middleware(['superadmin'])->group(function () {
-        Route::prefix('empresas')->name('empresas.')->group(function () {
-            Route::get('/', 'EmpresaController@index')->name('index');
-            Route::get('/create', 'EmpresaController@create')->name('create');
-            Route::get('/{id}/edit', 'EmpresaController@edit')->name('edit');
-            Route::post('/store', 'EmpresaController@store')->name('store');
-            Route::put('/{id}', 'EmpresaController@update')->name('update');
-            Route::delete('/{id}', 'EmpresaController@destroy')->name('destroy');
-        });
-    });*/
     Route::resource('empresas', 'EmpresaController');
     // condominios
-    Route::resource('condominios', 'CondominioController');
+    /*Route::resource('condominios', 'CondominioController');
     // imoveis
     Route::resource('imoveis', 'ImovelController');
     // moradores
@@ -75,86 +64,42 @@ Route::prefix('superadmin')->name('superadmin.')->group(function () {
     Route::resource('ocorrencias', 'OcorrenciaController');
     // reservas
     Route::resource('relatorios', 'RelatorioController');
+    */
         
 });
 
+##cadastros
+// empresas
+Route::resource('empresas', 'EmpresaController');
+// condominios
+Route::resource('condominios', 'CondominioController');
+// imoveis
+Route::resource('imoveis', 'ImovelController');
+// moradores
+Route::resource('moradores', 'MoradorController');
+// funcionarios
+Route::resource('funcionarios', 'FuncionarioController');
+// areas comuns
+Route::resource('areas-comuns', 'AreaComumController');
+// pets
+Route::resource('pets', 'PetController');
+// reservas
+Route::resource('reservas', 'ReservaController');
 
+##estacionamento/garagem
+// veiculos
+Route::resource('veiculos', 'VeiculoController');
+// vagas
+Route::resource('vagas', 'VagaController');
 
-
-
-Route::prefix('admin')->name('admin.')->group(function () {
-   ##cadastros
-    // imoveis
-    Route::resource('imoveis', 'ImovelController');
-    // moradores
-    Route::resource('moradores', 'MoradorController');
-    // funcionarios
-    Route::resource('funcionarios', 'FuncionarioController');
-    // areas comuns
-    Route::resource('areas-comuns', 'AreaComumController');
-    // pets
-    Route::resource('pets', 'PetController');
-    // reservas
-    Route::resource('reservas', 'ReservaController');
-
-    ##estacionamento/garagem
-    // veiculos
-    Route::resource('veiculos', 'VeiculoController');
-    // vagas
-    Route::resource('vagas', 'VagaController');
-
-    ##portarias
-    // entregas
-    Route::resource('entregas', 'EntregaController');
-    // visitantes
-    Route::resource('visitantes', 'VisitanteController');
-    // prestadores de servicos
-    Route::resource('prestadores-de-servicos', 'PrestadorDeServicoController');
-
-    ##usuarios/acessos
-    // usuarios
-    Route::resource('acessos/users', 'User\Auth\EditController');
-    // administradores
-    Route::resource('acessos/admins', 'Admin\Auth\EditController');
+##portarias
+// entregas
+Route::resource('entregas', 'EntregaController');
+// visitantes
+Route::resource('visitantes', 'VisitanteController');
+// prestadores de servicos
+Route::resource('prestadores-de-servicos', 'PrestadorDeServicoController');
         
-});
-
-
-
-
-
-// users
-Route::prefix('user')->name('user.')->group(function () {
-    
-    ##cadastros
-    // imoveis
-    Route::resource('imoveis', 'ImovelController');
-    // moradores
-    Route::resource('moradores', 'MoradorController');
-    // funcionarios
-    Route::resource('funcionarios', 'FuncionarioController');
-    // areas comuns
-    Route::resource('areas-comuns', 'AreaComumController');
-    // pets
-    Route::resource('pets', 'PetController');
-    // reservas
-    Route::resource('reservas', 'ReservaController');
-
-    ##estacionamento/garagem
-    // veiculos
-    Route::resource('veiculos', 'VeiculoController');
-    // vagas
-    Route::resource('vagas', 'VagaController');
-
-    ##portarias
-    // entregas
-    Route::resource('entregas', 'EntregaController');
-    // visitantes
-    Route::resource('visitantes', 'VisitanteController');
-    // prestadores de servicos
-    Route::resource('prestadores-de-servicos', 'PrestadorDeServicoController');
-        
-});
 
 
 
