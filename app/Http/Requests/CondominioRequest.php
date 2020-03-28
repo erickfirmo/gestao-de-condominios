e4d110e0f86f472cf31a8f765eaeb661fad73a7d
Érick Firmo <erickluques@gmail.com>
2020-03-28 01:16:15 -0300

    Update CondominioRequest.php

--- app/Http/Requests/CondominioRequest.php
+++ app/Http/Requests/CondominioRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class CondominioRequest extends FormRequest
 {
@@ -13,6 +14,12 @@ class CondominioRequest extends FormRequest
      */
     public function authorize()
     {
+        if(Auth::guard('admin')->check()){
+
+
+        } elseif($ee) {
+
+        }
         return true;
     }
 
