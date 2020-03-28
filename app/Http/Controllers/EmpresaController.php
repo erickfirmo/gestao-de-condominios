3d060700a604dc7d95000a1c1191d1c41ce194a8
Érick Firmo <erickluques@gmail.com>
2020-03-28 14:41:37 -0300

    Update EmpresaController.php

--- app/Http/Controllers/EmpresaController.php
+++ app/Http/Controllers/EmpresaController.php
@@ -12,7 +12,7 @@ class EmpresaController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:admin');
+        $this->middleware('auth:user');
         return $this->middleware('role');
 
     }   
