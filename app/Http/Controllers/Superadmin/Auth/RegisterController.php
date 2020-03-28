34876d915403fa482f116862eae3e572e504496f
Érick Firmo <erickluques@gmail.com>
2020-03-28 13:26:16 -0300

    Update RegisterController.php

--- app/Http/Controllers/Superadmin/Auth/RegisterController.php
+++ app/Http/Controllers/Superadmin/Auth/RegisterController.php
@@ -66,7 +66,6 @@ class RegisterController extends Controller
             'foto_de_perfil' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:superadmins'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
-            'api_token' => ['required', 'string', 'min:60', 'max:60'],
         ]);
     }
 
@@ -83,7 +82,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'foto_de_perfil' => $data['foto_de_perfil'],
             'password' => Hash::make($data['password']),
-            'api_token' => Str::random(60),
+            'role_id' => 1,
         ]);
     }
 }
