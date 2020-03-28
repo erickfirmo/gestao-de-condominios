7f1b7394b283053b2888c21304af7a4435fc38ab
Érick Firmo <erickluques@gmail.com>
2020-03-28 09:24:22 -0300

    Update 2014_02_28_112016_create_permissions_table.php

--- database/migrations/2014_02_28_112016_create_permissions_table.php
+++ database/migrations/2014_02_28_112016_create_permissions_table.php
@@ -14,7 +14,7 @@ class CreatePermissionsTable extends Migration
     public function up()
     {
         Schema::create('permissions', function (Blueprint $table) {
-            $table->increments('id');
+            $table->bigIncrements('id');
             $table->string('name')->nullable();
             $table->string('key')->nullable();
             $table->string('controller');
