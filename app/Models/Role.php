b080ac746d0bd8c04f5f292241a37e4e1043e200
Érick Firmo <erickluques@gmail.com>
2020-03-28 08:23:53 -0300

    Create 'Role' model

---
+++ app/Models/Role.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    public $table = 'roles';
+
+    public $fillable = [
+        'name',
+        'description'
+    ];
+}
