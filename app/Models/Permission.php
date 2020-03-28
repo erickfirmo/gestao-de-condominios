279fcc385f4bcfc7c1f0f26f13e65418703e7e26
Érick Firmo <erickluques@gmail.com>
2020-03-28 08:34:36 -0300

    Update Permission.php

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
-    //
+    public $table = 'permissions';
+
+    public $fillable = [
+        'name',
+        'key',
+        'controller',
+        'method',
+    ];
 }
