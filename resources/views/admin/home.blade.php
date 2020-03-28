8f4b8ea27216cb5c8647249b678b62a1d331509a
Érick Firmo <erickluques@gmail.com>
2020-03-28 14:04:35 -0300

    Create home.blade.php

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.dadmin')
+
+@section('content')
+
+@include('superadmin.partials._navbar')
+@include('superadmin.partials._sidebar')
+
+<!-- Main Container Start -->
+<main class="main--container">
+    <!-- Page Header Start -->
+    <section class="page--header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <!-- Page Title Start -->
+                    <h2 class="page--title h5">Dashboard</h2>
+                    <!-- Page Title End -->
+
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item active"><span>Dashboard</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Page Header End -->
+
+    @include('superadmin.partials._footer')
+</main>
+<!-- Main Container End -->
+@endsection
