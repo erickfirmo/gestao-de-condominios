c59c43d8ac31f7596a5c080ff3b9fad1840aa636
Érick Firmo <erickluques@gmail.com>
2020-03-28 14:03:35 -0300

    Update RegisterController.php

--- app/Http/Controllers/User/Auth/RegisterController.php
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/user/home';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
