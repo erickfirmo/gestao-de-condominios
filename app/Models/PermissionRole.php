5331f0d7bd487b010a6e68e3edd2f6228d98a375
Érick Firmo <erickluques@gmail.com>
2020-03-29 03:09:34 -0300

    Update PermissionRole.php

--- app/Models/PermissionRole.php
+++ app/Models/PermissionRole.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class PermissionRole extends Model
 {
-    public $table = 'permission_roles';
+    public $table = 'permission_role';
+
+    public $timestamps = false;
 
     public $fillable = [
         'role_id',
