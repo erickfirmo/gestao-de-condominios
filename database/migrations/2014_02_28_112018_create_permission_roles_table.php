e901299f55a003129015331195f55ffcea65ceff
Érick Firmo <erickluques@gmail.com>
2020-03-29 02:44:18 -0300

    Update 2014_02_28_112018_create_permission_roles_table.php

--- database/migrations/2014_02_28_112018_create_permission_roles_table.php
+++ database/migrations/2014_02_28_112018_create_permission_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePermissionRoleTable extends Migration
+class CreatePermissionRolesTable extends Migration
 {
     /**
      * Run the migrations.
