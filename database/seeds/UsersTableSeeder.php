7aa25b66a0de8debed9860dede5a88d86edbc71a
Érick Firmo <erickluques@gmail.com>
2020-03-29 01:50:33 -0300

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,28 +11,31 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $user = new App\User;
-        $user->name = 'Érick Firmo';
-        $user->email = 'erick@condominio.dev';
+        $user->name = 'Funcionario A';
+        $user->email = 'master@condominio.dev';
         $user->foto_de_perfil = '#';
         $user->password = bcrypt('psw');
+        $user->funcionario_id = 1;
         $user->role_id = 1;
         $user->remember_token = str_random(10);
         $user->save();
 
         $user = new App\User;
-        $user->name = 'Síndico';
+        $user->name = 'Funcionario B';
         $user->email = 'sindico@condominio.dev';
         $user->foto_de_perfil = '#';
         $user->password = bcrypt('psw');
+        $user->funcionario_id = 2;
         $user->role_id = 2;
         $user->remember_token = str_random(10);
         $user->save();
 
         $user = new App\User;
-        $user->name = 'Porteiro';
+        $user->name = 'Funcionario C';
         $user->email = 'porteiro@condominio.dev';
         $user->foto_de_perfil = '#';
         $user->password = bcrypt('psw');
+        $user->funcionario_id = 3;
         $user->role_id = 3;
         $user->remember_token = str_random(10);
         $user->save();
