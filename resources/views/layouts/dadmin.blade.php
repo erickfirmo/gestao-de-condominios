3832b72f6dcc2b54c66fce2540ada47168dc6496
Érick Firmo <erickluques@gmail.com>
2020-03-29 03:29:34 -0300

    Update dadmin.blade.php

--- resources/views/layouts/dadmin.blade.php
+++ resources/views/layouts/dadmin.blade.php
@@ -69,8 +69,6 @@
 <!-- Scripts -->
 
 @include('javascript._vars')
-@include('javascript._validation')
-
 <script src="{{ asset('themes/dadmin/assets/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('themes/dadmin/assets/js/jquery-ui.min.js') }}"></script>
 <script src="{{ asset('themes/dadmin/assets/js/bootstrap.bundle.min.js') }}"></script>
