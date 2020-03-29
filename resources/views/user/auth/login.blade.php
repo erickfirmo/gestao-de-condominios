60eb7c722aca9a656d1025d76e471d98d8285894
Érick Firmo <erickluques@gmail.com>
2020-03-29 03:26:05 -0300

    Update login.blade.php

--- resources/views/user/auth/login.blade.php
+++ resources/views/user/auth/login.blade.php
@@ -29,7 +29,7 @@
                                 <!-- Logo End -->
 
                                 <form action="{{ route('login') }}" method="POST">
-                                    <label class="m-account--title text-admin">ÁREA DE LOGIN</label>
+                                    <label class="m-account--title text-loginarea">ÁREA DE LOGIN</label>
                                     @csrf
 
                                     <div class="form-group">
@@ -63,7 +63,7 @@
                                     </div>
 
                                     <div class="m-account--actions">
-                                        <button type="submit" class="btn btn-rounded btn-info">Login</button>
+                                        <button type="submit" class="btn btn-rounded btn-warning">Login</button>
                                     </div>
 
                                     <!--<div class="m-account--alt">
