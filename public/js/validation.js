b24d9f97e296e23e6361eb23155479e63c444c2e
Érick Firmo <erickluques@gmail.com>
2020-03-30 06:04:44 -0300

    Update validation.js

--- public/js/validation.js
+++ public/js/validation.js
@@ -3,9 +3,7 @@ if(errors_response) {
         if(errors_response.hasOwnProperty(name))
         {
             let elem = document.getElementsByName(name)[0];
-            if(elem)
-            {
-
+            if(elem) {
                 elem.className += ' error';
                 elem.previousElementSibling.innerText = errors_response[name];
             }
