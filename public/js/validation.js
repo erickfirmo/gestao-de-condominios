6e81a2e6a6e4e5ccd2c4628a213eb4113c94fddc
Érick Firmo <erickluques@gmail.com>
2020-03-29 23:32:31 -0300

    Update validation.js

--- public/js/validation.js
+++ public/js/validation.js
@@ -1,19 +1,23 @@
-Object.keys(errors_response).forEach(function(name) {
-    let elem = document.getElementsByName(name)[0];
-    elem.className += ' error';
-    elem.previousElementSibling.innerText = errors_response[name];
-});
+if(errors_response) {
+    Object.keys(errors_response).forEach(function(name) {
+        if(errors_response.hasOwnProperty(name))
+        {
+            let elem = document.getElementsByName(name)[0];
+            elem.className += ' error';
+            elem.previousElementSibling.innerText = errors_response[name];
+        }
+    });
+}
 
-$('.error').onkeyup = function() {
+$('.error').keyup(function() {
     if($(this).val() != '') {
         $(this).removeClass('error');
         $(this).prev().text('');
     }
-};
+});
 
 //show form after load
 setTimeout(function() {
-    
     $('.show-onload').removeClass('d-none');
     $('.lds-ring').addClass('d-none')
 }, 0);
