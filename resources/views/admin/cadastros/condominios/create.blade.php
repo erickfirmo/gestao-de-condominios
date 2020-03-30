d1932aa6888740d49491370d58113d7c3741b276
Érick Firmo <erickluques@gmail.com>
2020-03-30 06:03:26 -0300

    Create create.blade.php

---
+++ resources/views/admin/cadastros/condominios/create.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.dadmin')
+
+@section('content')
+
+@include('admin.partials._navbar')
+@include('admin.partials._sidebar')
+
+<!-- Main Container Start -->
+<main class="main--container">
+
+    <!-- Page Header Start -->
+    <section class="page--header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <!-- Page Title Start -->
+                    <h2 class="page--title h5">Condomínios</h2>
+                    <!-- Page Title End -->
+
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ route('admin.condominios.index') }}">Condomínios</a></li>
+                        <li class="breadcrumb-item active"><span>Novo Cadastro</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Page Header End -->
+
+    <!-- Main Content Start -->
+    <section class="main--content">
+    
+        <div class="row gutter-20">
+            <div class="col-md-12">
+                <!-- Panel Start -->
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Novo Condomínio</h3>
+                    </div>
+
+                    <div class="panel-content">
+                        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+
+                    
+                        <form action="{{ route('admin.condominios.store') }}" method="POST" class="show-onload d-none">
+                            @csrf
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Nome</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <input type="text" name="nome" class="form-control" id="nome" maxlenght="60" value="{{ old('nome') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+                            
+                            <div class="row">
+                                <div class="col-lg-10 offset-lg-2">
+                                    <input type="submit" value="Salvar" class="btn btn-sm btn-rounded btn-success">
+                                    <a href="{{ route('admin.condominios.index') }}"><button type="button" class="btn btn-sm btn-rounded btn-outline-secondary">Cancelar</button></a>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+                <!-- Panel End -->
+            </div>
+        </div>
+    </section>
+
+    @include('admin.partials._footer')
+</main>
+
+<!-- Main Container End -->
+@endsection
+
