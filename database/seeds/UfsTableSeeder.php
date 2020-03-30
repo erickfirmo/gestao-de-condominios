3619a40ca4682402e400c86b58a12116bc3136b9
Érick Firmo <erickluques@gmail.com>
2020-03-30 07:06:02 -0300

    Update UfsTableSeeder.php

--- database/seeds/UfsTableSeeder.php
+++ database/seeds/UfsTableSeeder.php
@@ -11,13 +11,40 @@ class UfsTableSeeder extends Seeder
     public function run()
     {
         $ufs = [
+            'AC' => 'Acre',
+            'AL' => 'Alagoas',
+            'AP' => 'Amapá',
+            'AM' => 'Amazonas',
+            'BA' => 'Bahia',
+            'CE' => 'Ceará',
+            'DF' => 'Distrito Federal',
+            'ES' => 'Espírito Santo',
+            'GO' => 'Goiás',
+            'MA' => 'Maranhão',
+            'MT' => 'Mato Grosso',
+            'MS' => 'Mato Grosso do Sul',
+            'MG' => 'Minas Gerais',
+            'PA' => 'Pará',
+            'PB' => 'Paraíba',
+            'PR' => 'Paraná',
+            'PE' => 'Pernambuco',
+            'PI' => 'Piauí',
+            'RJ' => 'Rio de Janeiro',
+            'RN' => 'Rio Grande do Norte',
+            'RS' => 'Rio Grande do Sul',
+            'RO' => 'Rondônia',
+            'RR' => 'Roraima',
+            'SC' => 'Santa Catarina',
+            'SP' => 'São Paulo',
+            'SE' => 'Sergipe',
+            'TO' => 'Tocantins',
 
         ];
 
         foreach ($ufs as $sigla => $estado) {
             $uf = new App\Models\Uf;
-            $uf->sigla = '';
-            $uf->estado = '';
+            $uf->sigla = $sigla;
+            $uf->estado = $estado;
             $uf->save();
         }   
     }
