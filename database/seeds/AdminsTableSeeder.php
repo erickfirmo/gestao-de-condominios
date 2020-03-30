e821cd22a9c23935315cf58212e7c15baf1548a1
Érick Firmo <erickluques@gmail.com>
2020-03-30 01:07:59 -0300

    Update AdminsTableSeeder.php

--- database/seeds/AdminsTableSeeder.php
+++ database/seeds/AdminsTableSeeder.php
@@ -14,7 +14,7 @@ class AdminsTableSeeder extends Seeder
         $admin->name = 'Érick Firmo';
         $admin->email = 'root@erickfirmo.dev';
         $admin->foto_de_perfil = '#';
-        $admin->password = bcrypt('psw');
+        $admin->password = bcrypt('123456');
         $admin->role_id = 1;
         $admin->remember_token = str_random(10);
         $admin->save();
