948f148e6835e4a812af3ed94cec3823e4a1f2b9
Érick Firmo <erickluques@gmail.com>
2020-03-31 00:50:04 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,56 +16,27 @@ Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function () {
     Route::resource('empresas', 'EmpresaController');
     // condominios
     Route::resource('condominios', 'CondominioController');
-    // imoveis
-    Route::resource('imoveis', 'ImovelController');
-    // moradores
-    Route::resource('moradores', 'MoradorController');
-    // funcionarios
-    Route::resource('funcionarios', 'FuncionarioController');
-    // areas comuns
-    Route::resource('areas-comuns', 'AreaComumController');
-    // pets
-    Route::resource('pets', 'PetController');
-
-    ##estacionamento/garagem
-    // veiculos
-    Route::resource('veiculos', 'VeiculoController');
-    // vagas
-    Route::resource('vagas', 'VagaController');
-
-    ##portarias
-    // entregas
-    Route::resource('entregas', 'EntregaController');
-    // visitantes
-    Route::resource('visitantes', 'VisitanteController');
-    // prestadores de servicos
-    Route::resource('prestadores-de-servicos', 'PrestadorDeServicoController');
-
+    
     ##usuarios/acessos
     // usuarios
-    Route::resource('acessos/users', 'User\Auth\EditController');
+    Route::resource('acessos/users', 'Auth\EditController');
     // administradores
-    Route::resource('acessos/admins', 'Admin\Auth\EditController');
+    Route::resource('acessos/admins', 'Auth\EditController');
 
     ##financeiro
     // receita
-    Route::resource('financeiro/receitas', 'ReceitaController');
+    //Route::resource('financeiro/receitas', 'ReceitaController');
     // despesas
-    Route::resource('financeiro/despesas', 'DespesaController');
+    //Route::resource('financeiro/despesas', 'DespesaController');
     // faturas
-    Route::resource('financeiro/faturas', 'FaturaController');
+    //Route::resource('financeiro/faturas', 'FaturaController');
     // boletos
-    Route::resource('financeiro/boletos', 'BoletoController');
+    //Route::resource('financeiro/boletos', 'BoletoController');
 
     ##outros
-    // reservas
-    Route::resource('reservas', 'ReservaController');
-    // ocorrencias
-    Route::resource('ocorrencias', 'OcorrenciaController');
-    // reservas
-    Route::resource('relatorios', 'RelatorioController');
+    // relatorios
+    //Route::resource('relatorios', 'RelatorioController');
     
-        
 });
 
 ##cadastros
