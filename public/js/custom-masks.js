0d3490e5f68d112c421ec979aba87f1bc52f4e28
Érick Firmo <erickluques@gmail.com>
2020-04-01 12:20:19 -0300

    Update custom-masks.js

--- public/js/custom-masks.js
+++ public/js/custom-masks.js
@@ -2,6 +2,7 @@ $(document).ready(function($){
   
   $(".mask-cep").inputmask("99999-999");
   $(".mask-telefone").inputmask('99 9999-9999');
+  $(".mask-celular").inputmask('99 9999-9999');
   $(".mask-cnpj").inputmask('999.999.999/9999-99');
   //$(".mask-numero").inputmask('[^\d\\-]');
 });
