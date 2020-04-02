4b958556973f7219e5311ba1053a78a192a8071a
Érick Firmo <erickluques@gmail.com>
2020-04-02 05:48:00 -0300

    Update create.blade.php

--- resources/views/user/cadastros/imoveis/create.blade.php
+++ resources/views/user/cadastros/imoveis/create.blade.php
@@ -44,45 +44,6 @@
                     
                         <form action="{{ route('imoveis.store') }}" method="POST" class="show-onload d-none">
                             @csrf
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Nome</span>
-                                <div class="col-md-10">
-                                    <span class="form-text text-error"></span>
-                                    <input type="text" name="nome" class="form-control" id="nome" maxlenght="60" value="{{ old('nome') }}">
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Descrição</span>
-                                <div class="col-md-10">
-                                    <span class="form-text text-error"></span>
-                                    <input type="text" name="descricao" class="form-control" id="descricao" maxlenght="60" value="{{ old('descricao') }}">
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">CEP</span>
-                                <div class="col-md-10">
-                                    <span class="form-text text-error"></span>
-                                    <input type="text" name="cep" class="form-control mask-cep" id="cep" maxlenght="9" value="{{ old('cep') }}">
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Logradouro</span>
-                                <div class="col-md-10">
-                                    <span class="form-text text-error"></span>
-                                    <input type="text" name="logradouro" class="form-control" id="logradouro" maxlenght="40" value="{{ old('logradouro') }}">
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
@@ -96,69 +57,30 @@
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Bairro</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Bloco</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="bairro" class="form-control" id="bairro" maxlenght="40" value="{{ old('bairro') }}">
+                                    <input type="text" name="bloco" class="form-control" id="bloco" maxlenght="40" value="{{ old('bloco') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Cidade</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Andar</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="cidade" class="form-control" id="cidade" maxlenght="40" value="{{ old('cidade') }}">
+                                    <input type="text" name="andar" class="form-control" id="andar" maxlenght="40" value="{{ old('andar') }}">
                                 </div>
                             </div>
-                            <!-- Form Group End -->
+                            <!-- Form Group End -->                   
                             
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Estado</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Descrição</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <select name="uf_id" class="form-control" id="uf_id">
-                                        <option></option>
-                                        @foreach($ufs as $uf)
-                                            <option 
-                                            @if($uf->id == old('uf_id'))
-                                             {{ ' selected ' }}
-                                            @endif
-                                            value="{{ $uf->id }}">{{ $uf->estado }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-                            
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Condomínio</span>
-                                <div class="col-md-10">
-                                    <span class="form-text text-error"></span>
-                                    <select name="empresa_id" class="form-control" id="empresa_id">
-                                        <option></option>
-                                        @foreach($condominios as $condominio)
-                                            <option 
-                                            @if($condominio->id == old('condominio_id'))
-                                             {{ ' selected ' }}
-                                            @endif
-                                            value="{{ $condominio->id }}">{{ $condominio->nome }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-                            
-                            
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Complemento</span>
-                                <div class="col-md-10">
-                                    <span class="form-text text-error"></span>
-                                    <input type="text" name="complemento" class="form-control" id="complemento" maxlenght="40" value="{{ old('complemento') }}">
+                                    <input type="text" name="descricao" class="form-control" id="descricao" maxlenght="40" value="{{ old('descricao') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
