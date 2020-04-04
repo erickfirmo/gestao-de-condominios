784ff85d2a1f6caa84de50ed0f6fe2b72113d97b
Érick Firmo <erickluques@gmail.com>
2020-04-04 07:36:19 -0300

    Create edit.blade.php

---
+++ resources/views/user/cadastros/imoveis/edit.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.dadmin')
+
+@section('content')
+
+@include('user.partials._navbar')
+@include('user.partials._sidebar')
+
+<!-- Main Container Start -->
+<main class="main--container">
+    <!-- Page Header Start -->
+    <section class="page--header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <!-- Page Title Start -->
+                    <h2 class="page--title h5">Imóveis</h2>
+                    <!-- Page Title End -->
+
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ route('imoveis.index') }}">Imóveis</a></li>
+                        <li class="breadcrumb-item active"><span>Editar</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Page Header End -->
+
+    <!-- Main Content Start -->
+    <section class="main--content">
+        <div class="row gutter-20">
+            <div class="col-md-12">
+                <!-- Panel Start -->
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Editar Imóvel</h3>
+                        <form action="{{ route('imoveis.destroy', $imovel->id) }}" method="POST" class="remove-form" style="float:right">
+                            {{method_field('DELETE')}}
+                            <a href="#"><button class="btn btn-rounded btn-danger">Deletar Imóvel</button></a>
+                        </form>
+                    </div>
+
+                    <div class="panel-content">
+                        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+
+                        <form action="{{ route('imoveis.update', $imovel->id) }}" method="POST" class="show-onload d-none">
+                            @csrf
+                            {{ method_field('PUT') }}
+                            
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Número</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <input type="text" name="numero" class="form-control mask-numero" id="numero" maxlenght="10" value="{{ old('numero') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Bloco</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <input type="text" name="bloco" class="form-control" id="bloco" maxlenght="10" value="{{ old('bloco') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Andar</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <input type="text" name="andar" class="form-control" id="andar" maxlenght="3" value="{{ old('andar') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->                   
+                            
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Descrição</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <input type="text" name="descricao" class="form-control" id="descricao" maxlenght="200" value="{{ old('descricao') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Observações</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <textarea name="observacoes" class="form-control" maxlengh="200">{{ old('observacoes') }}</textarea>
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+                            
+                            <div class="row">
+                                <div class="col-lg-10 offset-lg-2">
+                                    <input type="submit" value="Salvar" class="btn btn-sm btn-rounded btn-success">
+                                    <a href="{{ route('imoveis.index') }}"><button type="button" class="btn btn-sm btn-rounded btn-outline-secondary">Cancelar</button></a>
+                                </div>
+                            </div>
+
+                        </form>
+                    </div>
+                </div>
+                <!-- Panel End -->
+            </div>
+        </div>
+    </section>
+
+    @include('user.partials._footer')
+</main>
+
+<!-- Main Container End -->
+@endsection
+
