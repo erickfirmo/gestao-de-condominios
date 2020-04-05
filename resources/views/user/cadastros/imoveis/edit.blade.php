a9f9e4cdf621c26607c776f13fc75ccec15c13b3
Érick Firmo <erickluques@gmail.com>
2020-04-05 19:24:21 -0300

    Update edit.blade.php

--- resources/views/user/cadastros/imoveis/edit.blade.php
+++ resources/views/user/cadastros/imoveis/edit.blade.php
@@ -52,7 +52,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Número</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="numero" class="form-control mask-numero" id="numero" maxlenght="10" value="{{ old('numero') }}">
+                                    <input type="text" name="numero" class="form-control mask-numero" id="numero" maxlenght="10" value="{{ $imovel->numero }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -62,7 +62,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Bloco</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="bloco" class="form-control" id="bloco" maxlenght="10" value="{{ old('bloco') }}">
+                                    <input type="text" name="bloco" class="form-control" id="bloco" maxlenght="10" value="{{ $imovel->bloco }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -72,17 +72,36 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Andar</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="andar" class="form-control" id="andar" maxlenght="3" value="{{ old('andar') }}">
+                                    <input type="text" name="andar" class="form-control" id="andar" maxlenght="3" value="{{ $imovel->andar }}">
                                 </div>
                             </div>
-                            <!-- Form Group End -->                   
+                            <!-- Form Group End -->      
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Condomínio</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <select name="condominio_id" class="form-control" id="condominio_id">
+                                        <option></option>
+                                        @foreach($condominios as $condominio)
+                                            <option 
+                                            @if($condominio->id == $condominio->id)
+                                             {{ ' selected ' }}
+                                            @endif
+                                            value="{{ $condominio->id }}">{{ $condominio->nome }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <!-- Form Group End -->               
                             
                             <!-- Form Group Start -->
                             <div class="form-group row">
                                 <span class="label-text col-md-2 col-form-label text-md-right">Descrição</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="descricao" class="form-control" id="descricao" maxlenght="200" value="{{ old('descricao') }}">
+                                    <input type="text" name="descricao" class="form-control" id="descricao" maxlenght="200" value="{{ $imovel->descricao }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -92,7 +111,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Observações</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <textarea name="observacoes" class="form-control" maxlengh="200">{{ old('observacoes') }}</textarea>
+                                    <textarea name="observacoes" class="form-control" maxlengh="200">{{ $imovel->observacoes }}</textarea>
                                 </div>
                             </div>
                             <!-- Form Group End -->
