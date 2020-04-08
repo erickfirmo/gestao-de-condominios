082835f2746b59e8606fbfed655ceee69e842c81
Érick Firmo <erickluques@gmail.com>
2020-04-08 16:57:15 -0300

    Update create.blade.php

--- resources/views/user/cadastros/areas-comuns/create.blade.php
+++ resources/views/user/cadastros/areas-comuns/create.blade.php
@@ -45,65 +45,63 @@
                         <form action="{{ route('areas-comuns.store') }}" method="POST" class="show-onload d-none">
                             @csrf
 
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
+                            
+                             <!-- Form Group Start -->
+                             <div class="form-group row">
                                 <span class="label-text col-md-2 col-form-label text-md-right">Nome</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="nome" class="form-control" id="nome" maxlenght="80" value="{{ old('nome') }}">
+                                    <input type="text" name="nome" class="form-control" id="nome" maxlenght="40" value="{{ old('nome') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right py-0">Gênero</span> 
-                                <div class="col-md-10 form-inline">
-                                    <label class="form-radio mr-3">
-                                        <input type="radio" name="genero" value="Masculino" class="form-radio-input" {{ old('genero') == 'Masculino' ? 'checked' : null }}>
-                                        <span class="form-radio-label">Masculino</span>
-                                    </label>
-                                    <label class="form-radio mr-3">
-                                        <input type="radio" name="genero" value="Feminino" class="form-radio-input" {{ old('genero') == 'Feminino' ? 'checked' : null }}>
-                                        <span class="form-radio-label">Feminino</span>
-                                    </label>
-                                    <label class="form-radio">
-                                        <input type="radio" name="genero" value="Não Definido" class="form-radio-input" {{ old('genero') == 'Não Definido' ? 'checked' : null }}>
-                                        <span class="form-radio-label">Não Definido</span>
-                                    </label>
-                                </div>
-                            </div>
-                            <!-- Form Group End -->               
-
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                            <span class="label-text col-md-2 col-form-label text-md-right">Imóvel</span>
-
+                                <span class="label-text col-md-2 col-form-label text-md-right">Abertura</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <select name="imovel_id" class="form-control" id="imovel_id">
-                                        <option></option>
-                                        @foreach($imoveis as $imovel)
-                                            <option value="{{ $imovel->id }}" {{ old('imovel_id') == $imovel->id ? ' selected' : null }}>
-                                                {{ 'Nº '.$imovel->numero.' - Bloco '.$imovel->bloco }}
-                                            </option>
-                                        @endforeach
-                                    </select>
+                                    <input type="time" name="abertura" class="form-control" id="abertura" maxlenght="20" value="{{ old('abertura') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
-                             <!-- Form Group Start -->
-                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right py-0">Proprietário do Imóvel</span> 
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Fechamento</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <input type="time" name="fechamento" class="form-control" id="fechamento" maxlenght="20" value="{{ old('fechamento') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right py-0">Status</span> 
                                 <div class="col-md-10 form-inline">
                                     <label class="form-radio mr-3">
-                                    <input type="radio" name="proprietario" value="1" class="form-radio-input" {{ old('proprietario') ? 'checked' : null }}>
-                                    <span class="form-radio-label">Sim</span></label>
+                                        <input type="radio" name="status" value="Aberto" class="form-radio-input" {{ old('status') == 'Aberto' ? 'checked' : null }}>
+                                        <span class="form-radio-label">Aberto</span>
+                                    </label>
+                                    <label class="form-radio mr-3">
+                                        <input type="radio" name="status" value="Fechado" class="form-radio-input" {{ old('status') == 'Fechado' ? 'checked' : null }}>
+                                        <span class="form-radio-label">Fechado</span>
+                                    </label>
                                     <label class="form-radio">
-                                    <input type="radio" name="proprietario" value="0" class="form-radio-input">
-                                    <span class="form-radio-label">Não</span>
-                                </label>
+                                        <input type="radio" name="status" value="Em Manutenção" class="form-radio-input" {{ old('status') == 'Em Manutenção' ? 'checked' : null }}>
+                                        <span class="form-radio-label">Em Manutenção</span>
+                                    </label>
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Descrição</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <input type="text" name="descricao" class="form-control" id="descricao" maxlenght="200" value="{{ old('descricao') }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
