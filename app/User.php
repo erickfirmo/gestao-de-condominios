6d10de0f6f6532c16683a66b9fa61b56e71792e9
Érick Firmo <erickluques@gmail.com>
2020-04-08 21:12:30 -0300

    Update User.php

--- app/User.php
+++ app/User.php
@@ -24,7 +24,6 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'foto_de_perfil',
         'password',
         'funcionario_id',
         'role_id',
