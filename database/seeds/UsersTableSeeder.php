d718dbdadba781c763396bbd0dc4cbef0a0d7f3c
Érick Firmo <erickluques@gmail.com>
2020-04-08 21:44:00 -0300

    Update UsersTableSeeder.php

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,33 +12,55 @@ class UsersTableSeeder extends Seeder
     {
         $user = new App\User;
         $user->name = 'Funcionario A';
-        $user->email = 'master@condominio.dev';
-        $user->foto_de_perfil = '#';
+        $user->email = 'master@condominio.loc';
+        $user->identidade = '99999999-1';
+        $user->genero = 'Masculino';
+        $user->entrada = '8:00';
+        $user->saida = '20:00';
+        $user->foto = '#';
+        $user->telefone_1 = '11999999999';
+        $user->telefone_2 = '';
+        $user->cargo = 'Master';
+        $user->condominio_id = 1;
         $user->password = bcrypt('123456');
-        $user->funcionario_id = 1;
         $user->role_id = 1;
         $user->remember_token = str_random(10);
         $user->save();
 
         $user = new App\User;
         $user->name = 'Funcionario B';
-        $user->email = 'sindico@condominio.dev';
-        $user->foto_de_perfil = '#';
+        $user->email = 'sindico@condominio.loc';
+        $user->identidade = '99999999-2';
+        $user->genero = 'Masculino';
+        $user->entrada = '8:00';
+        $user->saida = '20:00';
+        $user->foto = '#';
+        $user->telefone_1 = '11999999998';
+        $user->telefone_2 = '';
+        $user->cargo = 'Síndico';
+        $user->condominio_id = 1;
         $user->password = bcrypt('123456');
-        $user->funcionario_id = 2;
         $user->role_id = 2;
         $user->remember_token = str_random(10);
         $user->save();
 
         $user = new App\User;
         $user->name = 'Funcionario C';
-        $user->email = 'porteiro@condominio.dev';
-        $user->foto_de_perfil = '#';
+        $user->email = 'porteiro@condominio.loc';
+        $user->identidade = '99999999-3';
+        $user->genero = 'Masculino';
+        $user->entrada = '8:00';
+        $user->saida = '20:00';
+        $user->foto = '#';
+        $user->telefone_1 = '11999999997';
+        $user->telefone_2 = '';
+        $user->cargo = 'Porteiro';
+        $user->condominio_id = 1;
         $user->password = bcrypt('123456');
-        $user->funcionario_id = 3;
         $user->role_id = 3;
         $user->remember_token = str_random(10);
         $user->save();
+
 
     }
 
