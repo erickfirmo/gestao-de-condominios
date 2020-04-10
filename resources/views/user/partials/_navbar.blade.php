987808b39c59625587065032ab09845b1831caa0
Érick Firmo <erickluques@gmail.com>
2020-04-10 20:53:49 -0300

    Update _navbar.blade.php

--- resources/views/user/partials/_navbar.blade.php
+++ resources/views/user/partials/_navbar.blade.php
@@ -82,7 +82,7 @@
             <!-- Nav User Start -->
             <li class="nav-item dropdown nav--user online">
                 <a href="javascript:void(0)" class="nav-link" data-toggle="dropdown">
-                    <img src="{{ Auth::user()->foto_de_perfil != '#' ? Auth::user()->foto_de_perfil : '/images/profile-pic.png' }}" alt="" class="rounded-circle">
+                    <img src="{{ Auth::user()->foto_de_perfil != '' ? Auth::user()->foto_de_perfil : '/images/profile-pic.png' }}" alt="" class="rounded-circle">
                     <span>{{ Auth::user()->name }}</span>
                     <i class="fa fa-angle-down"></i>
                 </a>
