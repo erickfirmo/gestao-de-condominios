b565ede70e0bfe8ae918841921084b9aabe8f717
Érick Firmo <erickluques@gmail.com>
2020-04-11 12:01:08 -0300

    Update edit.blade.php

--- resources/views/user/auth/edit.blade.php
+++ resources/views/user/auth/edit.blade.php
@@ -26,11 +26,11 @@
 
                                     <div class="form-group">
                                         <div class="input-group">
-                                        <div class="input-group-prepend">
+                                        <div class="input-group-prepend" title="Nome">
                                                 <i class="fas fa-user"></i>
                                             </div>
 
-                                            <input placeholder="Digite o nome" id="nome" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ Auth::user()->name }}" required autofocus autocomplete="off">
+                                            <input title="Nome" placeholder="Digite o nome" id="nome" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ Auth::user()->name }}" required autofocus autocomplete="off">
                                             @if ($errors->has('name'))
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $errors->first('name') }}</strong>
@@ -38,14 +38,15 @@
                                             @endif
                                         </div>
                                     </div>
+                                    
 
                                     <div class="form-group">
                                         <div class="input-group">
-                                        <div class="input-group-prepend">
+                                        <div class="input-group-prepend" title="Foto de Perfil">
                                                 <i class="fas fa-images"></i>
                                             </div>
 
-                                            <input placeholder="URL da Foto de Perfil" id="foto_de_perfil" type="text" class="form-control{{ $errors->has('foto_de_perfil') ? ' is-invalid' : '' }}" name="foto_de_perfil" value="{{ Auth::user()->foto_de_perfil }}" required autofocus autocomplete="off">
+                                            <input title="Foto de Perfil" placeholder="URL da Foto de Perfil" id="foto_de_perfil" type="text" class="form-control{{ $errors->has('foto_de_perfil') ? ' is-invalid' : '' }}" name="foto_de_perfil" value="{{ Auth::user()->foto_de_perfil }}" autofocus autocomplete="off">
                                             @if ($errors->has('foto_de_perfil'))
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $errors->first('foto_de_perfil') }}</strong>
@@ -53,6 +54,119 @@
                                             @endif
                                         </div>
                                     </div>
+
+                                    
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                        <div class="input-group-prepend" title="Identidade">
+                                            <i class="fas fa-address-card"></i>
+                                            </div>
+
+                                            <input title="Identidade" placeholder="Digite a identidade" id="identidade" type="text" class="form-control{{ $errors->has('identidade') ? ' is-invalid' : '' }}" name="identidade" value="{{ Auth::user()->identidade }}" required autofocus autocomplete="off">
+                                            @if ($errors->has('identidade'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('identidade') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <!-- Form Group Start -->
+                                    <div class="form-group row">
+                                        <span class="label-text col-md-2 col-form-label text-md-right py-0">Gênero</span> 
+                                        <div class="col-md-10 form-inline">
+                                            <label class="form-radio mr-3">
+                                                <input type="radio" name="genero" value="Masculino" class="form-radio-input" {{ Auth::user()->genero == 'Masculino' ? 'checked' : null }}>
+                                                <span class="form-radio-label">Masculino</span>
+                                            </label>
+                                            <label class="form-radio mr-3">
+                                                <input type="radio" name="genero" value="Feminino" class="form-radio-input" {{ Auth::user()->genero == 'Feminino' ? 'checked' : null }}>
+                                                <span class="form-radio-label">Feminino</span>
+                                            </label>
+                                            <label class="form-radio">
+                                                <input type="radio" name="genero" value="Não Definido" class="form-radio-input" {{ Auth::user()->genero == 'Não Definido' ? 'checked' : null }}>
+                                                <span class="form-radio-label">Não Definido</span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <!-- Form Group End -->        
+                                    
+                                    <!-- Form Group Start -->
+                                    <div class="form-group row">
+                                        <span class="label-text col-md-2 col-form-label text-md-right">Entrada</span>
+                                        <div class="col-md-10">
+                                            <span class="form-text text-error"></span>
+                                            <input type="time" name="entrada" class="form-control" id="entrada" maxlenght="30" value="{{ Auth::user()->entrada }}">
+                                        </div>
+                                    </div>
+                                    <!-- Form Group End -->
+
+                                    <!-- Form Group Start -->
+                                    <div class="form-group row">
+                                        <span class="label-text col-md-2 col-form-label text-md-right">Saída</span>
+                                        <div class="col-md-10">
+                                            <span class="form-text text-error"></span>
+                                            <input type="time" name="saida" class="form-control" id="saida" maxlenght="30" value="{{ Auth::user()->saida }}">
+                                        </div>
+                                    </div>
+                                    <!-- Form Group End -->
+
+                                    <!-- Form Group Start -->
+                                    <div class="form-group row">
+                                        <span class="label-text col-md-2 col-form-label text-md-right">Foto</span>
+                                        <div class="col-md-10">
+                                            <span class="form-text text-error"></span>
+                                            <input type="text" name="foto" class="form-control" value="{{ Auth::user()->foto }}">
+                                        </div>
+                                    </div>
+                                    <!-- Form Group End -->
+                                     
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                        <div class="input-group-prepend" title="Telefone Principal">
+                                                <i class="fas fa-phone"></i>
+                                            </div>
+
+                                            <input title="Telefone Principal" placeholder="Digite o telefone principal" id="telefone_1" type="text" class="form-control{{ $errors->has('telefone_1') ? ' is-invalid' : '' }}" name="telefone_1" value="{{ Auth::user()->telefone_1 }}" required autofocus autocomplete="off">
+                                            @if ($errors->has('telefone_1'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('telefone_1') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                        <div class="input-group-prepend" title="Telefone de Emergência">
+                                                <i class="fas fa-phone"></i>
+                                            </div>
+
+                                            <input title="Telefone de Emergência" placeholder="Digite o telefone de emergência" id="telefone_2" type="text" class="form-control{{ $errors->has('telefone_2') ? ' is-invalid' : '' }}" name="telefone_2" value="{{ Auth::user()->telefone_2 }}" autofocus autocomplete="off">
+                                            @if ($errors->has('telefone_2'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('telefone_2') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                        <div class="input-group-prepend" title="Cargo">
+                                            <i class="fa fa-file"></i>
+                                            </div>
+
+                                            <input title="Cargo" placeholder="Digite o cargo" id="cargo" type="text" class="form-control{{ $errors->has('cargo') ? ' is-invalid' : '' }}" name="cargo" value="{{ Auth::user()->cargo }}" autofocus autocomplete="off">
+                                            @if ($errors->has('cargo'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('cargo') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                    </div>
+
+
 
                                     <div class="form-group">
                                         <div class="input-group">
