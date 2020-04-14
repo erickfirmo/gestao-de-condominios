6ccff4a64edf34fd64156df8cec86fc18b4071e7
Érick Firmo <erickluques@gmail.com>
2020-04-14 15:11:46 -0300

    Update user.php

--- routes/auth/user.php
+++ routes/auth/user.php
@@ -8,9 +8,18 @@ Route::middleware('auth:user')->group(function () {
 
 Route::prefix('usuarios')->name('usuarios.')->namespace('User')->group(function () {
     Auth::routes();
-    Route::get('create', 'Auth\CreateController@create')->name('create');
     Route::get('edit/{id}', 'Auth\EditController@edit')->name('edit');
     Route::post('update/{id}', 'Auth\EditController@update')->name('update');
+
+    ##usuarios/acessos
+    // usuarios
+    Route::get('/', 'Auth\ListController@index')->name('index');
+    Route::get('create', 'Auth\CreateController@create')->name('create');
+    Route::get('{id}/edit', 'Auth\EditController@edit')->name('edit');
+    Route::put('{id}/update', 'Auth\EditController@update')->name('update');
+    Route::delete('{id}/destroy', 'Auth\DeleteController@destroy')->name('destroy');
+
+
 
     Route::get('/password/email', function () {
         return redirect('login');
