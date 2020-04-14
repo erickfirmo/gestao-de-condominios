7d166ab0fc612fa9f892baf34fc8bd21a9020086
Érick Firmo <erickluques@gmail.com>
2020-04-14 15:44:59 -0300

    Create UserRequest.php

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * 
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        if($this->input('_method') == 'PUT' || $this->input('_method') == 'PATCH') {
+            return [
+                'nome_completo' => 'required|min:3|max:80',
+                'identidade' => 'required|min:1|max:11|unique:funcionarios,id,'.$this->route('id'),
+                'genero' => 'required|in:Masculino,Feminino,Não Definido',
+                'entrada' => 'required|min:1|max:30',
+                'saida' => 'required|min:1|max:30',
+                'telefone_1' => 'required|min:1|max:11',
+                'cargo' => 'required|min:1|max:30',
+            ];
+        } else {
+            return [
+                'nome_completo' => 'required|min:3|max:80',
+                'identidade' => 'required|min:1|max:11|unique:funcionarios',
+                'genero' => 'required|in:Masculino,Feminino,Não Definido',
+                'entrada' => 'required|min:1|max:30',
+                'saida' => 'required|min:1|max:30',
+                'telefone_1' => 'required|min:1|max:11',
+                'cargo' => 'required|min:1|max:30',
+            ];
+        }
+    }
+
+    public function attributes()
+    {
+        return [
+            'genero' => 'gênero',
+            'saida' => 'saída',
+        ];
+    }
+
+}
