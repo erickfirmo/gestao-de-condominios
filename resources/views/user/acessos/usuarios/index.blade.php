1e1fb3bcabd88f6f5714b84c2720bd0b67a5f9e1
Érick Firmo <erickluques@gmail.com>
2020-04-14 14:46:32 -0300

    Update index.blade.php

--- resources/views/user/acessos/usuarios/index.blade.php
+++ resources/views/user/acessos/usuarios/index.blade.php
@@ -12,11 +12,11 @@
                     <div class="row">
                         <div class="col-lg-6">
                             <!-- Page Title Start -->
-                            <h2 class="page--title h5">Funcionários</h2>
+                            <h2 class="page--title h5">Usuários</h2>
                             <!-- Page Title End -->
 
                             <ul class="breadcrumb">
-                                <li class="breadcrumb-item"><a href="{{ route('usuarios.index') }}">Funcionários</a></li>
+                                <li class="breadcrumb-item"><a href="{{ route('usuarios.index') }}">Usuários</a></li>
                                 <li class="breadcrumb-item active"><span>Todos</span></li>
                             </ul>
                         </div>
@@ -30,24 +30,21 @@
                 <div class="panel">
                     <!-- Records Header Start -->
                     <div class="records--header">
-
-
                         <div class="title fa-university">
-                            <h3 class="h3">Funcionários
+                            <h3 class="h3">Usuários
                             <a href="{{ route('usuarios.create') }}" class="btn btn-sm btn-outline-info">Adicionar Usuário</a>
                         
                             </h3>
                             <p>
                                 {{
                                     countMessage($usuarios, [
-                                        'zero' => 'Nenhum funcionário encontrado',
-                                        'one' => '1 funcionário encontrado',
+                                        'zero' => 'Nenhum usuários encontrado',
+                                        'one' => '1 usuários encontrado',
                                         'many' => '[X] usuários encontrados'
                                     ])
                                 }}
                             </p>
                         </div>
-
                         <div class="actions row">
                             <form class="search">
                                 <input type="text" class="form-control d-inline" placeholder="Buscar..." onkeyup="tableFilter('usuarios-table', this)">
@@ -60,15 +57,14 @@
 
                 <div class="panel">
 
-                    <div class="records--list" data-title="Lista de Funcionários">
+                    <div class="records--list" data-title="Lista de Usuários">
                         
                         <table id="recordsListView" class="usuarios-table">
                             <thead>
                                 <tr>
                                     <!--<th>ID</th>-->
-                                    <th>Nome Completo</th>
-                                    <th>Gênero</th>
-                                    <th>Identidade</th>
+                                    <th>Nome</th>
+                                    <th>Email</th>
                                     <th>Telefone(s)</th>
                                     <th>Cargo</th>
                                     <th class="not-sortable">Ações</th>
@@ -76,21 +72,16 @@
                             </thead>
                             <tbody>
                                 @foreach($usuarios as $usuario)
-
-                                <tr id="{{ 'funcionario-'.$usuario->id }}">
+                                <tr id="{{ 'usuario-'.$usuario->id }}">
                                     <!--<td>{ $usuario->id }</td>-->
                                     <td>{{ $usuario->name }}</td>
-                                    <td>{{ $usuario->genero }}</td>
-                                    <td>{{ $usuario->identidade }}</td>
+                                    <td>{{ $usuario->email }}</td>
                                     <td>{{ $usuario->telefone_1.($usuario->telefone_2 ? ', '.$usuario->telefone_2 : '') }}</td>
                                     <td>{{ $usuario->cargo }}</td>
                                     <td>
                                         <button class="d-inline mr-2 btn-action">
                                             <a href="{{ route('usuarios.edit', $usuario->id ) }}" class="btn-link"><i class="fa fa-pencil-alt"></i></a>
                                         </button>
-
-                                        
-
                                         <form action="{{ route('usuarios.destroy', $usuario->id) }}" method="POST" class="remove-form">
                                             @csrf
                                             {{method_field('DELETE')}}
