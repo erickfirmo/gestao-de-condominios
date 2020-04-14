b80576a182c516ee5f7618b0a2361a1811f37feb
Érick Firmo <erickluques@gmail.com>
2020-04-14 12:30:05 -0300

    Update edit.blade.php

--- resources/views/user/acessos/usuarios/edit.blade.php
+++ resources/views/user/acessos/usuarios/edit.blade.php
@@ -13,11 +13,11 @@
             <div class="row">
                 <div class="col-lg-6">
                     <!-- Page Title Start -->
-                    <h2 class="page--title h5">Funcionários</h2>
+                    <h2 class="page--title h5">Usuários</h2>
                     <!-- Page Title End -->
 
                     <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{ route('funcionarios.index') }}">Funcionários</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('usuarios.index') }}">Usuários</a></li>
                         <li class="breadcrumb-item active"><span>Editar</span></li>
                     </ul>
                 </div>
@@ -33,26 +33,26 @@
                 <!-- Panel Start -->
                 <div class="panel">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Editar Funcionário</h3>
-                        <form action="{{ route('funcionarios.destroy', $funcionario->id) }}" method="POST" class="remove-form" style="float:right">
+                        <h3 class="panel-title">Editar Usuário</h3>
+                        <form action="{{ route('usuarios.destroy', Auth::user()) }}" method="POST" class="remove-form" style="float:right">
                             {{method_field('DELETE')}}
-                            <a href="#"><button class="btn btn-rounded btn-danger">Deletar Funcionário</button></a>
+                            <a href="#"><button class="btn btn-rounded btn-danger">Deletar Usuário</button></a>
                         </form>
                     </div>
 
                     <div class="panel-content">
                         <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
 
-                        <form action="{{ route('funcionarios.update', $funcionario->id) }}" method="POST" class="show-onload d-none">
+                        <form action="{{ route('update', Auth::user()) }}" method="POST" class="show-onload d-none">
                             @csrf
                             {{ method_field('PUT') }}
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Nome Completo</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Nome</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="nome_completo" class="form-control" id="nome_completo" maxlenght="80" value="{{ $funcionario->nome_completo }}">
+                                    <input type="text" name="nome" class="form-control" id="nome" maxlenght="80" value="{{ Auth::user()->nome }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -62,7 +62,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Identidade</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="identidade" class="form-control" id="identidade" maxlenght="11" value="{{ $funcionario->identidade }}">
+                                    <input type="text" name="identidade" class="form-control" id="identidade" maxlenght="11" value="{{ Auth::user()->identidade }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -72,15 +72,15 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right py-0">Gênero</span> 
                                 <div class="col-md-10 form-inline">
                                     <label class="form-radio mr-3">
-                                        <input type="radio" name="genero" value="Masculino" class="form-radio-input" {{ $morador->genero == 'Masculino' ? 'checked' : null }}>
+                                        <input type="radio" name="genero" value="Masculino" class="form-radio-input" {{ Auth::user()->genero == 'Masculino' ? 'checked' : null }}>
                                         <span class="form-radio-label">Masculino</span>
                                     </label>
                                     <label class="form-radio mr-3">
-                                        <input type="radio" name="genero" value="Feminino" class="form-radio-input" {{ $morador->genero == 'Feminino' ? 'checked' : null }}>
+                                        <input type="radio" name="genero" value="Feminino" class="form-radio-input" {{ Auth::user()->genero == 'Feminino' ? 'checked' : null }}>
                                         <span class="form-radio-label">Feminino</span>
                                     </label>
                                     <label class="form-radio">
-                                        <input type="radio" name="genero" value="Não Definido" class="form-radio-input" {{ $morador->genero == 'Não Definido' ? 'checked' : null }}>
+                                        <input type="radio" name="genero" value="Não Definido" class="form-radio-input" {{ Auth::user()->genero == 'Não Definido' ? 'checked' : null }}>
                                         <span class="form-radio-label">Não Definido</span>
                                     </label>
                                 </div>
@@ -92,7 +92,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Entrada</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="time" name="entrada" class="form-control" id="entrada" maxlenght="30" value="{{ $funcionario->entrada }}">
+                                    <input type="time" name="entrada" class="form-control" id="entrada" maxlenght="30" value="{{ Auth::user()->entrada }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -102,7 +102,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Saída</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="time" name="saida" class="form-control" id="saida" maxlenght="30" value="{{ $funcionario->saida }}">
+                                    <input type="time" name="saida" class="form-control" id="saida" maxlenght="30" value="{{ Auth::user()->saida }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -112,7 +112,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Foto</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="foto" class="form-control" value="{{ $funcionario->foto }}">
+                                    <input type="text" name="foto" class="form-control" value="{{ Auth::user()->foto }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -122,7 +122,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Telefone 1</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="telefone_1" class="form-control" id="telefone_1" maxlenght="11" value="{{ $funcionario->telefone_1 }}">
+                                    <input type="text" name="telefone_1" class="form-control" id="telefone_1" maxlenght="11" value="{{ Auth::user()->telefone_1 }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -132,7 +132,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Telefone 2</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="telefone_2" class="form-control" id="telefone_2" maxlenght="11" value="{{ $funcionario->telefone_2 }}">
+                                    <input type="text" name="telefone_2" class="form-control" id="telefone_2" maxlenght="11" value="{{ Auth::user()->telefone_2 }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -142,7 +142,7 @@
                                 <span class="label-text col-md-2 col-form-label text-md-right">Cargo</span>
                                 <div class="col-md-10">
                                     <span class="form-text text-error"></span>
-                                    <input type="text" name="cargo" class="form-control" id="cargo" maxlenght="30" value="{{ $funcionario->cargo }}">
+                                    <input type="text" name="cargo" class="form-control" id="cargo" maxlenght="30" value="{{ Auth::user()->cargo }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->       
@@ -150,7 +150,7 @@
                             <div class="row">
                                 <div class="col-lg-10 offset-lg-2">
                                     <input type="submit" value="Salvar" class="btn btn-sm btn-rounded btn-success">
-                                    <a href="{{ route('funcionarios.index') }}"><button type="button" class="btn btn-sm btn-rounded btn-outline-secondary">Cancelar</button></a>
+                                    <a href="{{ route('usuarios.index') }}"><button type="button" class="btn btn-sm btn-rounded btn-outline-secondary">Cancelar</button></a>
                                 </div>
                             </div>
 
