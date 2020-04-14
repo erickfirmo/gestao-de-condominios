4be9953795c84c3a5f5c7a1078fe753fea32ee9b
Érick Firmo <erickluques@gmail.com>
2020-04-14 14:07:41 -0300

    Update user.php

--- routes/auth/user.php
+++ routes/auth/user.php
@@ -1,12 +1,13 @@
 <?php
 
 
-Route::prefix('')->name('')->namespace('User')->group(function () {
+Route::middleware('auth:user')->group(function () {
+    Route::get('/', 'User\HomeController@home')->name('/');
+    Route::get('home', 'User\HomeController@index')->name('home');
+});
+
+Route::prefix('usuarios')->name('usuarios.')->namespace('User')->group(function () {
     Auth::routes();
-    Route::middleware('auth:user')->group(function () {
-        Route::get('/', 'HomeController@home')->name('/');
-        Route::get('home', 'HomeController@index')->name('home');
-    });
     Route::get('edit/{id}', 'Auth\EditController@edit')->name('edit');
     Route::post('update/{id}', 'Auth\EditController@update')->name('update');
 
