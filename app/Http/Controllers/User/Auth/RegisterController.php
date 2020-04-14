348c0565a4fa2caab1818ccc1e2615c1bb69428b
Érick Firmo <erickluques@gmail.com>
2020-04-14 15:42:11 -0300

    Update RegisterController.php

--- app/Http/Controllers/User/Auth/RegisterController.php
+++ app/Http/Controllers/User/Auth/RegisterController.php
@@ -39,7 +39,7 @@ class RegisterController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:admin');
+        $this->middleware('auth:user');
     }
 
 
