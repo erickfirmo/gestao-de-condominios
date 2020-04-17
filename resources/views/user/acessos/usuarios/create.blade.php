6af37b355645939ae7f84a64fb45e2aab016ea99
Érick Firmo <erickluques@gmail.com>
2020-04-16 23:28:41 -0300

    Update create.blade.php

--- resources/views/user/acessos/usuarios/create.blade.php
+++ resources/views/user/acessos/usuarios/create.blade.php
@@ -77,26 +77,6 @@
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Senha</span>
-                                <div class="col-md-10">
-                                    <span id="password-text-error" class="form-text text-error"></span>
-                                    <input type="password" name="password" class="form-control" id="password" maxlenght="11" value="{{ old('password') }}">
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Confirmação de Senha</span>
-                                <div class="col-md-10">
-                                    <span id="password_confirmation-text-error" class="form-text text-error"></span>
-                                    <input type="password_confirmation" name="password_confirmation" class="form-control" id="password" maxlenght="11" value="{{ old('password') }}">
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
                                 <span class="label-text col-md-2 col-form-label text-md-right py-0">Gênero</span> 
                                 <div class="col-md-10 form-inline">
                                     <div class="d-block w-100">
@@ -192,6 +172,26 @@
                                             <option value="{{ $role->id }}">{{ ucwords($role->name) }}</option>
                                         @endforeach
                                     </select>
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Senha</span>
+                                <div class="col-md-10">
+                                    <span id="password-text-error" class="form-text text-error"></span>
+                                    <input type="password" name="password" class="form-control" id="password" maxlenght="40">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Confirmação de Senha</span>
+                                <div class="col-md-10">
+                                    <span id="password_confirmation-text-error" class="form-text text-error"></span>
+                                    <input type="password_confirmation" name="password_confirmation" class="form-control" id="password_confirmation" maxlenght="40">
                                 </div>
                             </div>
                             <!-- Form Group End -->
