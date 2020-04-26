77b779c14085c1e87a82593ab1562cc8d9e777e5
Érick Firmo <erickluques@gmail.com>
2020-04-26 14:35:43 -0300

    Update _navbar.blade.php

--- resources/views/user/partials/_navbar.blade.php
+++ resources/views/user/partials/_navbar.blade.php
@@ -89,7 +89,7 @@
 
                 <ul class="dropdown-menu">
                 <li><a href="{{ route('usuarios.edit', Auth::user()->id ) }}"><i class="far fa-user"></i>Minha Conta</a></li>
-                    <li><a href="{{ route('usuarios.register')}}"><i class="fas fa-users"></i>Cadastrar Usuário</a></li>
+                    <li><a href="{{ route('usuarios.create')}}"><i class="fas fa-users"></i>Cadastrar Usuário</a></li>
                     <!--li><a href="#"><i class="fas fa-users"></i>Cadastrar Administrador</a></li>-->
                     <li><a href="javascript:void(0)"><i class="fa fa-cog"></i>Configurações</a></li>
                     <li class="dropdown-divider"></li>
