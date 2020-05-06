5e9d4e0303cc56618dca798358f83ea946091813
Érick Firmo <erickluques@gmail.com>
2020-05-06 06:29:35 -0300

    Update EntregaController.php

--- app/Http/Controllers/User/EntregaController.php
+++ app/Http/Controllers/User/EntregaController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\EntregaRequest;
+use App\Models\Entrega;
+use Illuminate\Support\Facades\Auth;
 
 class EntregaController extends Controller
 {
