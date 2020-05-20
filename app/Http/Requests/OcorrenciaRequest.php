82dc098661264cdfac90a33e5ef72225568679a1
Érick Firmo <erickluques@gmail.com>
2020-05-20 17:09:52 -0300

    Create OcorrenciaRequest.php

---
+++ app/Http/Requests/OcorrenciaRequest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class OcorrenciaRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+        
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * 
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        if($this->input('_method') == 'PUT' || $this->input('_method') == 'PATCH') {
+            return [
+                'descricao' => 'required|min:1|max:200',
+                // 'status' => 'required|in:a,b',
+                'agora' => 'required|in:0,1',
+                'data' => 'required_if:agora,0',
+                'hora' => 'required_if:agora,0',
+                'status' => 'required',
+                'gravidade' => 'required|in:baixa,media,alta',
+                'morador_id' => 'required|min:1|max:20|numeric',
+            ];
+        } else {
+            return [
+                'descricao' => 'required|min:1|max:200',
+                // 'status' => 'required|in:a,b',
+                'agora' => 'required|in:0,1',
+                'data' => 'required_if:agora,1',
+                'hora' => 'required_if:agora,1',
+                'gravidade' => 'required|in:baixa,media,alta',
+                'morador_id' => 'required|min:1|max:20|numeric',
+            ];
+        }
+    }
+
+    public function attributes()
+    {
+        return [
+            'descricao' => 'descrição',
+            'morador_id' => 'morador'
+        ];
+    }
+
+}
