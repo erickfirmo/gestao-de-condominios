0600e11cf41a7088397db226712b66bc8705c589
Érick Firmo <erickluques@gmail.com>
2020-05-20 17:09:56 -0300

    Create PrestadorDeServicoRequest.php

---
+++ app/Http/Requests/PrestadorDeServicoRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+
+class PrestadorDeServicoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+        
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     * 
+     *
+     * @return array
+     */
+    public function rules()
+    {
+
+        if($this->input('_method') == 'PUT' || $this->input('_method') == 'PATCH') {
+            return [
+                'nome',
+                'chegada',
+                'saida',
+                'identidade',
+                'morador_id',
+            ];
+        } else {
+            return [
+                'nome',
+                'chegada',
+                'saida',
+                'identidade',
+                'morador_id',
+            ];
+        }
+    }
+
+    public function attributes()
+    {
+        return [
+            'saida' => 'saída',
+            'morador_id' => 'morador',
+        ];
+    }
+
+}
