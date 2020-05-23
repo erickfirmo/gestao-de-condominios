1e4e671ada6e86d287c98eb36e1552d0c3488328
Érick Firmo <erickluques@gmail.com>
2020-05-23 18:48:17 -0300

    Update OcorrenciaController.php

--- app/Http/Controllers/User/OcorrenciaController.php
+++ app/Http/Controllers/User/OcorrenciaController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\OcorrenciaRequest;
+use App\Models\Ocorrencia;
+use Illuminate\Support\Facades\Auth;
 
 class OcorrenciaController extends Controller
 {
+    public function __construct()
+    {
+        return $this->middleware('auth:user');
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,9 @@ class OcorrenciaController extends Controller
      */
     public function index()
     {
-        //
+        $ocorrencias = Ocorrencia::all();
+
+        return view('user.cadastros.ocorrencias.index', [ 'ocorrencias' => $ocorrencias]);
     }
 
     /**
@@ -24,18 +34,37 @@ class OcorrenciaController extends Controller
      */
     public function create()
     {
-        //
+        return view('user.cadastros.ocorrencias.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\OcorrenciaRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(OcorrenciaRequest $request)
     {
-        //
+        $request->validated();
+
+        $descricao = $request->descricao;
+        $status = $request->status;
+        $data = $request->data;
+        $hora = $request->hora;
+        $gravidade = $request->gravidade;
+        $morador_id = $request->morador_id;
+
+        $ocorrencia = new Ocorrencia;
+        $ocorrencia->descricao = $descricao;
+        $ocorrencia->status = $status;
+        $ocorrencia->data = $data;
+        $ocorrencia->hora = $hora;
+        $ocorrencia->gravidade = $gravidade;
+        $ocorrencia->morador_id = $morador_id;
+        $ocorrencia->save();
+
+        return redirect()->route('ocorrencias.edit', compact('ocorrencia'))
+            ->with('success', 'Ocorrência cadastrada com sucesso!');
     }
 
     /**
@@ -46,7 +75,9 @@ class OcorrenciaController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('user.cadastros.ocorrencias.show', [
+            'ocorrencia' => Ocorrencia::findOrFail($id),
+        ]);
     }
 
     /**
@@ -57,19 +88,33 @@ class OcorrenciaController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('user.cadastros.ocorrencias.edit', [
+            'ocorrencia' => Ocorrencia::findOrFail($id),
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\OcorrenciaRequest  $requestH
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(OcorrenciaRequest $request, $id)
     {
-        //
+        $request->validated();
+
+        $ocorrencia = Ocorrencia::findOrFail($id)->update([
+            'descricao' => $request->descricao,
+            'status' => $request->status,
+            'data' => $request->data,
+            'hora' => $request->hora,
+            'gravidade' => $request->gravidade,
+            'morador_id' => $request->morador_id,
+        ]);
+
+        return redirect()->route('ocorrencias.edit', compact('ocorrencia'))
+            ->with('success', 'Informações da ocorrência atualizadas com sucesso!');
     }
 
     /**
@@ -80,6 +125,9 @@ class OcorrenciaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Ocorrencia::findOrFail($id)->delete();
+
+        return redirect()->route('ocorrencias.index')
+            ->with('success', 'Ocorrência removida com sucesso!');
     }
 }
