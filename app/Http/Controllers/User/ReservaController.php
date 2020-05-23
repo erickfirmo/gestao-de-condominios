a63387e42f07261ac08d89e5e4cb47551a4405f6
Érick Firmo <erickluques@gmail.com>
2020-05-23 17:51:47 -0300

    Update ReservaController.php

--- app/Http/Controllers/User/ReservaController.php
+++ app/Http/Controllers/User/ReservaController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\ReservaRequest;
+use App\Models\Reserva;
+use Illuminate\Support\Facades\Auth;
 
 class ReservaController extends Controller
 {
+    public function __construct()
+    {
+        return $this->middleware('auth:user');
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,9 @@ class ReservaController extends Controller
      */
     public function index()
     {
-        //
+        $reservas = Reserva::all();
+
+        return view('user.cadastros.reservas.index', [ 'reservas' => $reservas]);
     }
 
     /**
@@ -24,18 +34,37 @@ class ReservaController extends Controller
      */
     public function create()
     {
-        //
+        return view('user.cadastros.reservas.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\ReservaRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ReservaRequest $request)
     {
-        //
+        $request->validated();
+
+        $observacoes = $request->observacoes;
+        $inicio = $request->inicio;
+        $termino = $request->termino;
+        $status = $request->status;
+        $area_comum_id = $request->area_comum_id;
+        $morador_id = $request->morador_id;
+
+        $reserva = new Reserva;
+        $reserva->observacoes = $observacoes;
+        $reserva->inicio = $inicio;
+        $reserva->termino = $termino;
+        $reserva->status = $status;
+        $reserva->area_comum_id = $area_comum_id;
+        $reserva->morador_id = $morador_id;
+        $reserva->save();
+
+        return redirect()->route('reservas.edit', compact('reserva'))
+        ->with('success', 'Reserva cadastrada com sucesso!');
     }
 
     /**
@@ -46,7 +75,9 @@ class ReservaController extends Controller
      */
     public function show($id)
     {
-        //
+        return view('user.cadastros.reservas.show', [
+            'reserva' => Reserva::findOrFail($id)
+        ]);
     }
 
     /**
@@ -57,19 +88,33 @@ class ReservaController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('user.cadastros.reservas.edit', [
+            'reserva' => Reserva::findOrFail($id),
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\ReservaRequest  $requestH
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ReservaRequest $request, $id)
     {
-        //
+        $request->validated();
+
+        $reserva = Reserva::findOrFail($id)->update([
+            'observacoes' => $request->observacoes,
+            'inicio' => $request->inicio,
+            'termino' => $request->termino,
+            'status' => $request->status,
+            'area_comum_id' => $request->area_comum_id,
+            'morador_id' => $request->morador_id,
+        ]);
+
+        return redirect()->route('reservas.edit', compact('reserva'))
+            ->with('success', 'Informações da reserva atualizados com sucesso!');
     }
 
     /**
@@ -80,6 +125,9 @@ class ReservaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Reserva::findOrFail($id)->delete();
+
+        return redirect()->route('reservas.index')
+            ->with('success', 'Reserva removida com sucesso!');
     }
 }
