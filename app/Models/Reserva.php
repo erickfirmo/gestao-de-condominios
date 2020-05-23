6f13764ae21fa11c5cd8eb5b5a540336d3f4cee5
Érick Firmo <erickluques@gmail.com>
2020-05-23 17:36:42 -0300

    Update Reserva.php

--- app/Models/Reserva.php
+++ app/Models/Reserva.php
@@ -15,7 +15,6 @@ class Reserva extends Model
         'status',
         'area_comum_id',
         'morador_id',
-        'funcionario_id',
     ];
 
     public function area_comum()
@@ -26,11 +25,6 @@ class Reserva extends Model
     public function morador()
     {
         return $this->belongsTo(Morador::class, 'morador_id');
-    }
-
-    public function funcionario()
-    {
-        return $this->belongsTo(Funcionario::class, 'funcionario_id');
     }
 
     public function relatorios()
