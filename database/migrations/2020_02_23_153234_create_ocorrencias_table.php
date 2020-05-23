7ea8a7f41119bf8d0ab5da6ad83b4730910f5bfd
Érick Firmo <erickluques@gmail.com>
2020-05-23 18:48:27 -0300

    Update 2020_02_23_153234_create_ocorrencias_table.php

--- database/migrations/2020_02_23_153234_create_ocorrencias_table.php
+++ database/migrations/2020_02_23_153234_create_ocorrencias_table.php
@@ -19,12 +19,6 @@ class CreateOcorrenciasTable extends Migration
             $table->string('data', 20);
             $table->string('hora', 20);
             $table->enum('gravidade', ['baixa', 'media', 'alta']);
-
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users')
-                ->onDelete('cascade');
                 
             $table->unsignedBigInteger('morador_id');
             $table->foreign('morador_id')
