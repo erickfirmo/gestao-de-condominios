99b41cc0953a9e36939f2e56876334b8223c702d
Érick Firmo <erickluques@gmail.com>
2020-06-03 21:34:30 -0300

    Update edit.blade.php

--- resources/views/user/cadastros/moradores/edit.blade.php
+++ resources/views/user/cadastros/moradores/edit.blade.php
@@ -13,11 +13,11 @@
             <div class="row">
                 <div class="col-lg-6">
                     <!-- Page Title Start -->
-                    <h2 class="page--title h5">Ocorrências</h2>
+                    <h2 class="page--title h5">Moradores</h2>
                     <!-- Page Title End -->
 
                     <ul class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{ route('ocorrencias.index') }}">Ocorrências</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('moradores.index') }}">Moradores</a></li>
                         <li class="breadcrumb-item active"><span>Editar</span></li>
                     </ul>
                 </div>
@@ -33,75 +33,88 @@
                 <!-- Panel Start -->
                 <div class="panel">
                     <div class="panel-heading">
-                        <h3 class="panel-title">Editar Ocorrência</h3>
-                        <form action="{{ route('ocorrencias.destroy', $ocorrencia->id) }}" method="POST" class="remove-form" style="float:right">
+                        <h3 class="panel-title">Editar Morador</h3>
+                        <form action="{{ route('moradores.destroy', $morador->id) }}" method="POST" class="remove-form" style="float:right">
                             {{method_field('DELETE')}}
-                            <a href="#"><button class="btn btn-rounded btn-danger">Deletar Ocorrência</button></a>
+                            <a href="#"><button class="btn btn-rounded btn-danger">Deletar Morador</button></a>
                         </form>
                     </div>
 
                     <div class="panel-content">
                         <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
 
-                        <form action="{{ route('ocorrencias.update', $ocorrencia->id) }}" method="POST" class="show-onload d-none">
+                        <form action="{{ route('moradores.update', $morador->id) }}" method="POST" class="show-onload d-none">
                             @csrf
                             {{ method_field('PUT') }}
 
 
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                            <span class="label-text col-md-2 col-form-label text-md-right">Nome</span>
+                             <!-- Form Group Start -->
+                             <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Nome</span>
                                 <div class="col-md-10">
                                     <span id="nome-text-error" class="form-text text-error"></span>
-                                    <input type="text" name="nome" class="form-control" id="nome" maxlenght="80" value="{{ $ocorrencia->nome }}">
+                                    <input type="text" name="nome" class="form-control" id="nome" maxlenght="80" value="{{ $morador->nome }}">
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
-                            
+                            <!-- Form Group Start -->
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Gravidade</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Gênero</span>
                                 <div class="col-md-10">
-                                    <span id="gravidade-text-error" class="form-text text-error"></span>
-                                    <select name="gravidade" id="gravidade" class="form-control">
+                                    <span id="genero-text-error" class="form-text text-error"></span>
+                                    <select name="genero" id="genero" class="form-control">
                                         <option></option>
-                                        <option value="baixa" {{ selectOption('baixa', $ocorrencia->gravidade) }}>Baixa</option>
-                                        <option value="média" {{ selectOption('média', $ocorrencia->gravidade) }}>Média</option>
-                                        <option value="1alta" {{ selectOption('alta', $ocorrencia->gravidade) }}>Alta</option>
+                                        <option value="Masculino" {{ selectOption('Masculino', $morador->genero) }}>Masculino</option>
+                                        <option value="Feminino" {{ selectOption('Feminino', $morador->genero) }}>Feminino</option>
+                                        <option value="Não Definido" {{ selectOption('Não Definido', $morador->genero) }}>Não Definido</option>
                                     </select>
                                 </div>
                             </div>
-                            <!-- Form Group End -->    
+                            <!-- Form Group End -->       
 
-                            
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Data</span>
+                                <span class="label-text col-md-2 col-form-label text-md-right">Imóvel</span>
+
                                 <div class="col-md-10">
-                                    <span class="form-text text-error"></span>
-                                    <input type="date" name="data" class="form-control" id="data" value="{{ $ocorrencia->data }}">
+                                    <span id="imovel_id-text-error" class="form-text text-error"></span>
+                                    <select name="imovel_id" class="form-control" id="imovel_id">
+                                        <option></option>
+                                        @foreach($imoveis as $imovel)
+                                            <option value="{{ $imovel->id }}" {{ $imovel->id == $morador->imovel->id ? ' selected' : null}}>
+                                                {{ 'Nº '.$imovel->numero.' - Bloco '.$imovel->bloco }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                             <!-- Form Group Start -->
+                             <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right py-0">Proprietário do Imóvel</span> 
+                                <div class="col-md-10 form-inline">
+                                    <span id="proprietario-text-error" class="form-text text-error"></span>
+                                    <label class="form-radio mr-3">
+                                    <input type="radio" name="proprietario" value="1" class="form-radio-input" {{ $morador->proprietario ? 'checked' : null }}>
+                                    <span class="form-radio-label">Sim</span></label>
+                                    <label class="form-radio">
+                                    <input type="radio" name="proprietario" value="0" class="form-radio-input" {{ !$morador->proprietario ? 'checked' : null }}>
+                                    <span class="form-radio-label">Não</span>
+                                </label>
                                 </div>
                             </div>
                             <!-- Form Group End -->
 
                             <!-- Form Group Start -->
                             <div class="form-group row">
-                                <span class="label-text col-md-2 col-form-label text-md-right">Hora</span>
-                                <div class="col-md-10">
-                                    <span class="form-text text-error"></span>
-                                    <input type="time" name="hora" class="form-control" id="hora" value="{{ $ocorrencia->hora }}">
-                                </div>
-                            </div>
-                            <!-- Form Group End -->
-
-                            <!-- Form Group Start -->
-                            <div class="form-group row">
-                            <span class="label-text col-md-2 col-form-label text-md-right">Descrição</span>
+                            <span class="label-text col-md-2 col-form-label text-md-right">Observações</span>
 
                                 <div class="col-md-10">
-                                    <span id="descricao-text-error" class="form-text text-error"></span>
-                                    <textarea name="descricao" class="form-control" maxlengh="400">{{ $ocorrencia->descricao }}</textarea>
+                                    <span id="observacoes-text-error" class="form-text text-error"></span>
+                                    <textarea name="observacoes" class="form-control" maxlengh="400">{{ $morador->observacoes }}</textarea>
                                 </div>
                             </div>
                             <!-- Form Group End -->
@@ -109,7 +122,7 @@
                             <div class="row">
                                 <div class="col-lg-10 offset-lg-2">
                                     <input type="submit" value="Salvar" class="btn btn-sm btn-rounded btn-success">
-                                    <a href="{{ route('ocorrencias.index') }}"><button type="button" class="btn btn-sm btn-rounded btn-outline-secondary">Cancelar</button></a>
+                                    <a href="{{ route('moradores.index') }}"><button type="button" class="btn btn-sm btn-rounded btn-outline-secondary">Cancelar</button></a>
                                 </div>
                             </div>
 
