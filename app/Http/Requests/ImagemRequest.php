5cf4b7be155060d2ff82466db3d5d688c741a3da
Érick Firmo <erickluques@gmail.com>
2020-06-05 18:52:59 -0300

    Update ImagemRequest.php

--- app/Http/Requests/ImagemRequest.php
+++ app/Http/Requests/ImagemRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ImageRequest extends FormRequest
+class ImagemRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
