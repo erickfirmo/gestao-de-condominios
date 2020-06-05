ee181aa0431f4da6878a454b047fcff0529b4a53
Érick Firmo <erickluques@gmail.com>
2020-06-05 19:21:33 -0300

    Create index.blade.php

---
+++ resources/views/user/cadastros/imagens/index.blade.php
@@ -0,0 +1,80 @@
+@extends('layouts.dadmin')
+
+@section('content')
+
+@include('user.partials._navbar')
+@include('user.partials._sidebar')
+<!-- Main Container Start -->
+<main class="main--container">
+            <!-- Page Header Start -->
+            <section class="page--header">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <!-- Page Title Start -->
+                            <h2 class="page--title h5">Galeria de Imagens</h2>
+                            <!-- Page Title End -->
+
+                            <ul class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="{{ route('imagens.index') }}">Imagens</a></li>
+                                <li class="breadcrumb-item active"><span>Todas</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Page Header End -->
+
+            <!-- Main Content Start -->
+            <section class="main--content">
+                <div class="panel">
+                    <!-- Records Header Start -->
+                    <div class="records--header">
+
+
+                        <div class="title fa-image">
+                            <h3 class="h3">Galeria de Imagens
+                            <a href="{{ route('imagens.create') }}" class="btn btn-sm btn-outline-info">Adicionar Imagem</a>
+                        
+                            </h3>
+                            <p>
+                                {{
+                                    countMessage($imagens, [
+                                        'zero' => 'Nenhuma imagem encontrada',
+                                        'one' => '1 imagem encontrada',
+                                        'many' => '[X] imagens encontradas'
+                                    ])
+                                }}
+                            </p>
+                        </div>
+
+                        <!--<div class="actions row">
+                            <form class="search">
+                                <input type="text" class="form-control d-inline" placeholder="Buscar..." onkeyup="tableFilter('imagens-table', this)">
+                                <button type="submit" class="btn btn-rounded"><i class="fa fa-search"></i></button>
+                            </form>
+                        </div>-->
+                    </div>
+                    <!-- Records Header End -->
+                </div>
+
+                <div class="panel">
+                    <div class="galeria-de-imagens" data-title="Galeria de Imagens">
+                        <div class="imagem">
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Main Content End -->
+
+    @include('user.partials._footer')
+</main>
+<!-- Main Container End -->
+
+@push('js')
+    <script src="{{ asset('js/table-filter.js') }}"></script>
+@endpush
+
+@endsection
+
