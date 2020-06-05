66c22515a2518894647243dd80b0c8a6c4d42e27
Érick Firmo <erickluques@gmail.com>
2020-06-05 18:53:20 -0300

    Update 2020_06_03_051223_create_imagens_table.php

--- database/migrations/2020_06_03_051223_create_imagens_table.php
+++ database/migrations/2020_06_03_051223_create_imagens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateImagesTable extends Migration
+class CreateImagensTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateImagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
+        Schema::create('imagens', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('original_name');
             $table->string('name')->nullable();
@@ -30,6 +30,6 @@ class CreateImagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('images');
+        Schema::dropIfExists('imagens');
     }
 }
