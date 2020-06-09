f00811518d358893a026273ccd5dfbc1c812307f
Érick Firmo <erickluques@gmail.com>
2020-06-09 01:32:04 -0300

    Update ImagemRequest.php

--- app/Http/Requests/ImagemRequest.php
+++ app/Http/Requests/ImagemRequest.php
@@ -24,7 +24,7 @@ class ImagemRequest extends FormRequest
     public function rules()
     {
         return [
-            ''
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ];
     }
 }
