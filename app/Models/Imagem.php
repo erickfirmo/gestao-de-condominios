3deb727fc8ea700063358a94852eaaf96e658c41
Érick Firmo <erickluques@gmail.com>
2020-06-09 18:21:34 -0300

    Update Imagem.php

--- app/Models/Imagem.php
+++ app/Models/Imagem.php
@@ -12,6 +12,26 @@ class Imagem extends Model
         'orginal_name',
         'name',
         'extensao',
+        'title',
+        'alt',
         'size',
     ];
+
+    /*
+    public static function renameFile($file_name, $new_file_name, $instituicao_prefixo)
+    {
+        rename(public_path("imagens/".strtolower($instituicao_prefixo)."/$file_name.html"), public_path("imagens/".strtolower($instituicao_prefixo)."/$new_file_name.html"));
+    }
+
+    public static function createFile($file_name, $content ,$instituicao_prefixo)
+    {
+        file_put_contents(public_path("imagens/".strtolower($instituicao_prefixo)."/$file_name.html"), $content);
+    }
+
+    public static function deleteFile($file_name, $instituicao_prefixo)
+    {
+        unlink(public_path("imagens/".strtolower($instituicao_prefixo)."/$file_name.html"));
+    }
+    */
+
 }
