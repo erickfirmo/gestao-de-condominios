3173f38aa3571454b2c8caaf281367d58164cc45
Érick Firmo <erickluques@gmail.com>
2020-06-09 04:14:55 -0300

    Update functions.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,13 +1,9 @@
 //return json with input values from form by id
-function getFormDataById(formId)
-{
-    let formData = {};
-    let form = document.getElementById(formId);
-    let inputs = form.getElementsByTagName('input');
-    inputs.concat((form.getElementsByTagName('textarea')))
-    inputs.map(function(input) {
-        formData[input.name] = input.value; 
-    });
-    // console.log(formData);
+function getFormDataById(formId) {
+    var formData = {};
+    var inputs = document.getElementById(formId).getElementsByTagName('input');
+    var textareas = document.getElementById(formId).getElementsByTagName('textarea');
+    for (let i = 0; i < inputs.length; i++) { formData[inputs[i].name] = inputs[i].value; }
+    for (let i = 0; i < textareas.length; i++) { formData[textareas[i].name] = textareas[i].value; }
     return formData;
 }
