0171575783009a96734e90500860b6dd1e5826ce
Érick Firmo <erickluques@gmail.com>
2020-06-16 01:15:18 -0300

    Create create.blade.php

---
+++ resources/views/user/cadastros/imagens/create.blade.php
@@ -0,0 +1,112 @@
+@extends('layouts.dadmin')
+
+@section('content')
+
+@include('user.partials._navbar')
+@include('user.partials._sidebar')
+
+<!-- Main Container Start -->
+<main class="main--container">
+
+    <!-- Page Header Start -->
+    <section class="page--header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6">
+                    <!-- Page Title Start -->
+                    <h2 class="page--title h5">Moradores</h2>
+                    <!-- Page Title End -->
+
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ route('visitantes.index') }}">Moradores</a></li>
+                        <li class="breadcrumb-item active"><span>Novo Cadastro</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Page Header End -->
+
+    <!-- Main Content Start -->
+    <section class="main--content">
+    
+        <div class="row gutter-20">
+            <div class="col-md-12">
+                <!-- Panel Start -->
+                <div class="panel">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Novo Morador</h3>
+                    </div>
+
+                    <div class="panel-content">
+                        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+
+                    
+                        <form action="{{ route('visitantes.store') }}" method="POST" class="show-onload d-none">
+                            @csrf
+
+                           <!-- Form Group Start -->
+                           <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Nome</span>
+                                <div class="col-md-10">
+                                    <span class="form-text text-error"></span>
+                                    <input type="text" name="nome" class="form-control" id="nome" maxlenght="80" value="{{ old('nome') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Entrada</span>
+                                <div class="col-md-3">
+                                    <span id="entrada-text-error"  class="form-text text-error"></span>
+                                    <input type="time" name="entrada" class="form-control" id="entrada" value="{{ old('entrada') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Saída</span>
+                                <div class="col-md-3">
+                                    <span id="saida-text-error"  class="form-text text-error"></span>
+                                    <input type="time" name="saida" class="form-control" id="saida" value="{{ old('saida') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Identidade</span>
+                                <div class="col-md-10">
+                                    <span id="identidade-text-error"  class="form-text text-error"></span>
+                                    <input type="text" name="identidade" class="form-control" id="identidade" maxlenght="11" value="{{ old('identidade') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                            <!-- Form Group Start -->
+                            <div class="form-group row">
+                                <span class="label-text col-md-2 col-form-label text-md-right">Morador</span>
+                                <div class="col-md-10">
+                                    <span id="morador_id-text-error"  class="form-text text-error"></span>
+                                    <input type="text" name="morador" class="form-control autocomplete-morador" id="morador" maxlenght="40" value="{{ old('morador') }}">
+                                    <input type="text" name="morador_id" class="form-control" id="morador_id" maxlenght="8" value="{{ old('morador_id') }}">
+                                </div>
+                            </div>
+                            <!-- Form Group End -->
+
+                        </form>
+                    </div>
+                </div>
+                <!-- Panel End -->
+            </div>
+        </div>
+    </section>
+
+    @include('user.partials._footer')
+</main>
+
+<!-- Main Container End -->
+@endsection
+
