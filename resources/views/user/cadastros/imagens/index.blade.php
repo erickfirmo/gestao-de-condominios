c2a2ddd3e2964235dcd2990f8208c6db4ec01e34
Érick Firmo <erickluques@gmail.com>
2020-07-08 00:50:43 -0300

    Update index.blade.php

--- resources/views/user/cadastros/imagens/index.blade.php
+++ resources/views/user/cadastros/imagens/index.blade.php
@@ -63,15 +63,21 @@
                 <div class="panel">
                     <div class="galeria-de-imagens" data-title="Galeria de Imagens">
                         <div class="row" id="boxGallery">
-                        @foreach($images as $image)
-                            <div id="image_{{ $image->id }}" title="{{ $image->original_name }}" style="background-image: url('upload/images/{{ $image->original_name }}')" class="imagem col-sm-3 col-md-3 d-inline">
-                                <div class="image-actions">
-                                    <span id="delete_image_{{ $image->id }}" class="delete-image" title="Deletar">
-                                        <i class="fa fa-trash"></i>
-                                    </span>
+                        @if(count($images))
+                            @foreach($images as $image)
+                                <div id="image_{{ $image->id }}" title="{{ $image->original_name }}" style="background-image: url('upload/images/{{ $image->original_name }}')" class="imagem col-sm-3 col-md-3 d-inline">
+                                    <div class="image-actions">
+                                        <span id="delete_image_{{ $image->id }}" class="delete-image" title="Deletar">
+                                            <i class="fa fa-trash"></i>
+                                        </span>
+                                    </div>
                                 </div>
+                            @endforeach
+                        @else
+                            <div class="col-sm-12 col-md-12 d-inline">
+                                <p>Nenhuma imagem encontrada</p>
                             </div>
-                        @endforeach
+                        @endif
                         </div>
                     </div>
                 </div>
@@ -158,7 +164,10 @@
                     for (let i = 0; i < response.uploaded_images.length; i++) {
                         new_images = new_images + '<div id="image_'+response.uploaded_images[i].id+'" title="'+response.uploaded_images[i].file_name+'" style="background-image: url('+"'"+response.uploaded_images[i].url+"'"+')" class="imagem col-sm-6 col-md-3 d-inline"><div class="image-actions"><span id="delete_image_'+response.uploaded_images[i].id+'" class="delete-image"><i class="fa fa-trash"></i></span></div></div>';
                     }
+                    
                     let old_images = $('#boxGallery').html();
+                    old_images = old_images == '<div class="col-sm-12 col-md-12 d-inline"><p>Nenhuma imagem encontrada</p></div>' ? old_images : '';
+
                     $('#boxGallery').html(new_images + old_images);
                     $('#uploadImageModal').modal('hide');
                     $('#previewBox').html('');
@@ -167,7 +176,6 @@
                         response.success,
                         'success'
                     );
-
 
                     $('.delete-image').on('click', function() {
 
