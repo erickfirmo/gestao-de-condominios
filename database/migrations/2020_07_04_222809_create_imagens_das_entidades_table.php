c18b011acb495f14a1cdedef5c4d98d7040e5686
Érick Firmo <erickluques@gmail.com>
2020-07-08 00:50:39 -0300

    Create 2020_07_04_222809_create_imagens_das_entidades_table.php

---
+++ database/migrations/2020_07_04_222809_create_imagens_das_entidades_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagensDasEntidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('imagens_das_entidades', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            
+            $table->unsignedBigInteger('imagem_id');
+            $table->foreign('imagem_id')
+                ->references('id')
+                ->on('imagens')
+                ->onDelete('cascade');
+
+            // entidade_id
+            $table->unsignedBigInteger('parent_id');
+            $table->string('parent_table', 30);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('imagens_das_entidades');
+    }
+}
