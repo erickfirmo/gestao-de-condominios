52f3c657e959bec5507be488838e968f23501b98
Érick Firmo <erickluques@gmail.com>
2020-07-09 23:58:36 -0300

    Update ImagemDaEntidade.php

--- app/Models/ImagemDaEntidade.php
+++ app/Models/ImagemDaEntidade.php
@@ -11,11 +11,18 @@ class ImagemDaEntidade extends Model
     public $fillable = [
         'imagem_id',
         'parent_id',
-        'parent_table'
+        'parent_class'
     ];
 
     public function imagem()
     {
         return $this->belongsTo(Imagem::class, 'imagem_id');
     }
+
+    public function parent()
+    {
+        return $this->belongsTo($this->fillable['parent_class'], 'parent_id');
+    }
+
 }
+
