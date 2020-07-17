531e21a7ad54a2e7c39f0abc21fe95fba2ceaf2e
Érick Firmo <erickluques@gmail.com>
2020-07-17 19:18:45 -0300

    Update ImagemDaEntidadeController.php

--- app/Http/Controllers/User/ImagemDaEntidadeController.php
+++ app/Http/Controllers/User/ImagemDaEntidadeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\ImagemDaEntidade;
 
 class ImagemDaEntidadeController extends Controller
 {
@@ -12,8 +13,24 @@ class ImagemDaEntidadeController extends Controller
         return $this->middleware('auth:user');
     }
 
-    public function destroy()
+    public function destroy(Request $request)
     {
-        
+        $images_ids = $requets->input('images_ids');
+        $images_ids = json_decode($images_ids);
+        $removed_images = [];
+
+        foreach($images_ids as $id)
+        {
+            $imagem_da_entidade = ImagemDaEntidade::findOrFail($id);
+            $imagem_da_entidade->delete();    
+            array_push($removed_images, $id);
+        }
+
+        $response_message =  count($images_ids) > 1 ? 'Imagens removidas com sucesso!' : 'Imagem removida com sucesso!';
+
+        return response()->json([
+            'success' => $response_message,
+            'removed_images' => $removed_images,
+        ], 201);
     }
 }
