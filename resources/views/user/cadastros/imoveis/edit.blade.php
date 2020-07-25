1d37e581ef9bf024db9892eca4a72450d202f351
Érick Firmo <erickluques@gmail.com>
2020-07-25 19:36:26 -0300

    Update edit.blade.php

--- resources/views/user/cadastros/imoveis/edit.blade.php
+++ resources/views/user/cadastros/imoveis/edit.blade.php
@@ -47,42 +47,12 @@
                             @csrf
                             {{ method_field('PUT') }}
 
-                            <!-- Form Group Start -->
-                            <div class="p-0" data-title="Imagens do Imóvel">
-                                <div class="row">
-                                    <span class="label-text col-md-2 text-md-right">Fotos do Imóvel</span>
-                                    <div class="col-md-10 m-0">
-                                        <div class="flex-container single-gallery">
-                                            @if(count($imovel->imagens()->get()))
-                                                @foreach($imovel->imagens()->get() as $key => $imagem)
-                                                    <div id="parent_image_{{ $imagem->id }}" title="{{ $imagem->imagem()->get()[0]->original_name }}" style="background-image: url('/upload/images/{{ $imagem->imagem()->get()[0]->original_name }}')" class="image-thumbnail" data-image="/upload/images/{{ $imagem->imagem()->get()[0]->original_name }}">
-                                                        <div class="image-actions">
-                                                            <span id="remove_parent_image_{{ $imagem->id }}" class="delete-image" title="Remover Imagem">
-                                                                <i class="fa fa-times"></i>
-                                                            </span>
-                                                        </div>
-                                                    </div>
-                                                @endforeach
-                                            @else
-                                                <div class="col-sm-12 col-md-12 d-inline">
-                                                    <p>Nenhuma imagem encontrada</p>
-                                                </div>
-                                            @endif
-                                            <div id="addImage" title="Adicionar Imagem" class="image-thumbnail border border-info rounded pointer">
-                                                <div>
-                                                    <span class="d-block mx-auto text-center text-info" style="font-size: 26px;">
-                                                        <i class="fa fa-plus"></i>
-                                                    </span>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-
-
-                            <!-- Form Group End -->
+                            <!-- Imagens da Entidade Start -->
+                            @include('components._single-gallery', [
+                                'title' => 'Fotos do Imóvel',
+                                'images' => $imovel->imagens()->get()
+                            ])
+                            <!-- Imagens da Entidade End -->
                             
                             <!-- Form Group Start -->
                             <div class="form-group row">
