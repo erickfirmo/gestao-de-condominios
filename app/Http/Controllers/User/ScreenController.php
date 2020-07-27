4a8f9cf6d1ade813869bf33e669cad208d0909f5
Érick Firmo <erickluques@gmail.com>
2020-07-27 03:15:00 -0300

    Create ScreenController.php

---
+++ app/Http/Controllers/User/ScreenController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Session;
+use Illuminate\Support\Facades\Auth;
+
+class ScreenController extends Controller
+{
+    
+    
+
+}
