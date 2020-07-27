30b3d659acb13233b453043e1a5efa3ac7c40f74
Érick Firmo <erickluques@gmail.com>
2020-07-27 08:34:14 -0300

    Update full-gallery.js

--- public/js/full-gallery.js
+++ public/js/full-gallery.js
@@ -40,11 +40,11 @@
                 success: function(response) {
                     let new_images = '';
                     for (let i = 0; i < response.uploaded_images.length; i++) {
-                        new_images = new_images + '<div id="image_'+response.uploaded_images[i].id+'" title="'+response.uploaded_images[i].file_name+'" style="background-image: url('+"'"+response.uploaded_images[i].url+"'"+')" class="imagem col-sm-6 col-md-3 d-inline"><div class="image-actions"><span id="delete_image_'+response.uploaded_images[i].id+'" class="delete-image"><i class="fa fa-trash"></i></span></div></div>';
+                        new_images = new_images + '<div id="image_'+response.uploaded_images[i].id+'" title="'+response.uploaded_images[i].file_name+'" style="background-image: url('+"'"+response.uploaded_images[i].url+"'"+')" class="image-thumbnail" data-image="'+response.uploaded_images[i].url+'"><div class="image-actions"><span id="delete_image_'+response.uploaded_images[i].id+'" class="delete-image"><i class="fa fa-times"></i></span></div></div>';
                     }
                     
                     let old_images = $('#boxGallery').html();
-                    old_images = old_images == '<div class="col-sm-12 col-md-12 d-inline"><p>Nenhuma imagem encontrada</p></div>' ? old_images : '';
+                    /*old_images = old_images == '<div class="col-sm-12 col-md-12 d-inline"><p>Nenhuma imagem encontrada</p></div>' ? old_images : '';*/
 
                     $('#boxGallery').html(new_images + old_images);
                     $('#uploadImageModal').modal('hide');
