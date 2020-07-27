afce6d28181b95d1f4516d79b0452fa91d23ee8f
Érick Firmo <erickluques@gmail.com>
2020-07-27 03:54:54 -0300

    Create LockScreen.php

---
+++ app/Http/Middleware/LockScreen.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+use Session;
+
+class LockScreen
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if (Auth::check()) {
+
+           
+
+        } else {
+            
+            return $next($request);
+
+        }
+
+    }
+}
