10d91b4e1937e8215c0e04fd748eccbe51e721be
Érick Firmo <erickluques@gmail.com>
2020-07-27 03:15:44 -0300

    Update ScreenController.php

--- app/Http/Controllers/User/ScreenController.php
+++ app/Http/Controllers/User/ScreenController.php
@@ -9,7 +9,34 @@ use Illuminate\Support\Facades\Auth;
 
 class ScreenController extends Controller
 {
-    
-    
+    public function __construct()
+    {
+        return $this->middleware('auth:user');
+    }
+
+    public function lock()
+    {
+        if(Session::has('lock_screen'))
+        {
+            $screen_obj = Session::get('lock_screen');
+
+            if($screen_obj['encrypt_password'] == md5(Auth::user()->password) && $screen_obj['lock'] == false)
+            {
+                $screen_obj = [
+                    'access' => true,
+                    'lock' => md5(Auth::user()->password),
+                ];
+                Session::put('lock_screen', $screen_obj);
+            }
+
+        } else {
+            $screen_obj = [
+                'lock' => true,
+                'encrypt_password' => md5(Auth::user()->password),
+            ];
+            Session::put('lock_screen', $screen_obj);
+        }
+        
+    }
 
 }
