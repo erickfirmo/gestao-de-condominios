a20e0680bdbb2d7e0767074564fec1992fc7f30b
Érick Firmo <erickluques@gmail.com>
2020-07-27 02:04:59 -0300

    Update ImagensDasEntidadesTableSeeder.php

--- database/seeds/ImagensDasEntidadesTableSeeder.php
+++ database/seeds/ImagensDasEntidadesTableSeeder.php
@@ -13,25 +13,25 @@ class ImagensDasEntidadesTableSeeder extends Seeder
         $imagem_da_entidade = new App\Models\ImagemDaEntidade;
         $imagem_da_entidade->imagem_id = '1';
         $imagem_da_entidade->parent_id = '1';
-        $imagem_da_entidade->parent_class = 'Imovel';
+        $imagem_da_entidade->parent_class = 'App\Models\Imovel';
         $imagem_da_entidade->save();
-        
+
         $imagem_da_entidade = new App\Models\ImagemDaEntidade;
         $imagem_da_entidade->imagem_id = '2';
         $imagem_da_entidade->parent_id = '1';
-        $imagem_da_entidade->parent_class = 'Imovel';
+        $imagem_da_entidade->parent_class = 'App\Models\Imovel';
         $imagem_da_entidade->save();
 
         $imagem_da_entidade = new App\Models\ImagemDaEntidade;
         $imagem_da_entidade->imagem_id = '3';
         $imagem_da_entidade->parent_id = '2';
-        $imagem_da_entidade->parent_class = 'Imovel';
+        $imagem_da_entidade->parent_class = 'App\Models\Imovel';
         $imagem_da_entidade->save();
 
         $imagem_da_entidade = new App\Models\ImagemDaEntidade;
         $imagem_da_entidade->imagem_id = '3';
         $imagem_da_entidade->parent_id = '3';
-        $imagem_da_entidade->parent_class = 'Imovel';
+        $imagem_da_entidade->parent_class = 'App\Models\Imovel';
         $imagem_da_entidade->save();
     }
 }
