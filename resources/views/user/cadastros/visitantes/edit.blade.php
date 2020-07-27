522e75f7db505ba733e91e191e9f38e39dcbffa2
Érick Firmo <erickluques@gmail.com>
2020-07-27 02:16:58 -0300

    Update edit.blade.php

--- resources/views/user/cadastros/visitantes/edit.blade.php
+++ resources/views/user/cadastros/visitantes/edit.blade.php
@@ -35,6 +35,7 @@
                     <div class="panel-heading">
                         <h3 class="panel-title">Editar Visitante</h3>
                         <form action="{{ route('visitantes.destroy', $visitante->id) }}" method="POST" class="remove-form" style="float:right">
+                            @csrf
                             {{method_field('DELETE')}}
                             <a href="#"><button class="btn btn-rounded btn-danger">Deletar Visitante</button></a>
                         </form>
